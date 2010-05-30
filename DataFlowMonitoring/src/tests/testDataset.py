5cd8cc2eedf6249f05ee8ca288947a3ea71417df
kreczko <>
2010-05-30 20:15:45 +0000

    - Changed behaviour of constructor (no exception) - added validation functions - added construction functions - made class variables public instead of creating getters and setters

--- DataFlowMonitoring/src/tests/testDataset.py
+++ DataFlowMonitoring/src/tests/testDataset.py
@@ -11,29 +11,50 @@ import unittest
 correctDatasetMC = '/WJets-madgraph/Summer09-MC_31X_V3_7TeV-v3/GEN-SIM-RECO'
 correctDatasetMCSpring10 = '/WJets-madgraph/Spring10-START3X_V26_S09-v1/GEN-SIM-RECO'
 realData = '/EG/Run2010A-PromptReco-v1/RECO'
-Summer09Twikipage = 'https://twiki.cern.ch/twiki/bin/view/CMS/ProductionSummer2009at7TeV'
-Spring10Twikipage = 'https://twiki.cern.ch/twiki/bin/viewauth/CMS/ProductionReProcessingSpring10'
-LumiTwiki2010Data = 'https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki2010Data'
-falseDataset = 'this_dataset_does_not_exist'
-datasetMC = Dataset(correctDatasetMC)
-datasetMCSpring10 = Dataset(correctDatasetMCSpring10)
-dataset = Dataset(realData)
+
+Summer09Twikipage = Dataset.Summer09Twikipage
+Spring10Twikipage = Dataset.Spring10Twikipage
+LumiTwiki2010Data = Dataset.LumiTwiki2010Data
+
+invalidDatasetName = 'this_dataset_does_not_exist'
+datasetMC = Dataset.createDatasetFromUserInput(correctDatasetMC)
+datasetMCSpring10 = Dataset.createDatasetFromUserInput(correctDatasetMCSpring10)
+dataset = Dataset.createDatasetFromUserInput(realData)
+invalidDataset = Dataset('')
+
+validSQLResult = {'name': realData, 'twikiPage': LumiTwiki2010Data, 'isData':True}
+invalidSQLResultNoTwiki = {'name': realData, 'twikiPage': '', 'isData':True}
+invalidSQLResultNoDataflag = {'name': realData, 'twikiPage': LumiTwiki2010Data, 'isData':None}
+validDBDataset = Dataset.createDatasetFromSQLResult(validSQLResult)
+validDBDataset2 = Dataset.createDatasetFromSQLResult(validSQLResult)
+invalidDBDatasetNoTwikiPage = Dataset.createDatasetFromSQLResult(invalidSQLResultNoTwiki)
+invalidDBDatasetNoDataFlag = Dataset.createDatasetFromSQLResult(invalidSQLResultNoDataflag)
 
 class TestDataset(unittest.TestCase):
         
     def testDatasetExists(self):
-        self.assertFalse(Dataset.exists(falseDataset))
+        self.assertFalse(Dataset.exists(invalidDatasetName))
         self.assertTrue(Dataset.exists(correctDatasetMC))
         
-    def testConstructorInvalidDatasetRaisesException(self):
-        self.failUnlessRaises(IOError, Dataset, falseDataset)
+    def testSetDatasetInvalidDatasetRaisesException(self):
+        self.failUnlessRaises(IOError, dataset.createDatasetFromUserInput, invalidDatasetName)
         
     def testIsDatasetRealDataOrSimulation(self):
-        self.assertFalse(datasetMC.isData())
-        self.assertTrue(dataset.isData())
+        self.assertFalse(datasetMC.isData)
+        self.assertTrue(dataset.isData)
         
     def testDatasetTwikiPage(self):
-        self.assertEquals(Spring10Twikipage, datasetMCSpring10.TwikiPage())
-        self.assertEquals(Summer09Twikipage, datasetMC.TwikiPage())
-        self.assertEquals(LumiTwiki2010Data, dataset.TwikiPage())
+        self.assertEquals(Spring10Twikipage, datasetMCSpring10.TwikiPage)
+        self.assertEquals(Summer09Twikipage, datasetMC.TwikiPage)
+        self.assertEquals(LumiTwiki2010Data, dataset.TwikiPage)
+    
+    def testIsValdiDataset(self):
+        self.assertTrue(dataset.isValid())
+        self.assertTrue(datasetMC.isValid())
+        self.assertFalse(invalidDataset.isValid())
+        self.assertFalse(invalidDBDatasetNoTwikiPage.isValid())
+        self.assertFalse(invalidDBDatasetNoDataFlag.isValid())
         
+    def testCompare(self):
+        self.assertEquals(validDBDataset, validDBDataset2)
+        self.assertTrue(datasetMC > dataset)
--- DataFlowMonitoring/src/tools/Dataset.py
+++ DataFlowMonitoring/src/tools/Dataset.py
@@ -13,32 +13,28 @@ class Dataset(object):
     Summer09Twikipage = 'https://twiki.cern.ch/twiki/bin/view/CMS/ProductionSummer2009at7TeV'
     Spring10Twikipage = 'https://twiki.cern.ch/twiki/bin/viewauth/CMS/ProductionReProcessingSpring10'
     LumiTwiki2010Data = 'https://twiki.cern.ch/twiki/bin/view/CMS/LumiWiki2010Data'
+    validTwikis = [Summer09Twikipage, Spring10Twikipage, LumiTwiki2010Data]
     
     def __init__(self, name):
-        if not Dataset.exists(name):
-            raise IOError
-        self.__name = name
-        self.__isData = self.__getDataType() == 'data'
-        self.__selectTwikiPage()
-    
+        self.Name = name
+        self.isData = None
+        self.TwikiPage = None
+        
     def __getDataType(self):
-        searchstring = "find datatype where dataset =" + self.__name
+        searchstring = "find datatype where dataset =" + self.Name
         return Dataset.search(searchstring)[0]
     
     def __selectTwikiPage(self):
-        if 'Summer09' in self.__name:
-            self.__twikipage = Dataset.Summer09Twikipage
-        elif 'Spring10' in self.__name:
-            self.__twikipage = Dataset.Spring10Twikipage
+        if 'Summer09' in self.Name:
+            self.TwikiPage = Dataset.Summer09Twikipage
+        elif 'Spring10' in self.Name:
+            self.TwikiPage = Dataset.Spring10Twikipage
         else:
-            self.__twikipage = Dataset.LumiTwiki2010Data 
+            self.TwikiPage = Dataset.LumiTwiki2010Data 
     
-    def isData(self):
-        return self.__isData
-    
-    def TwikiPage(self):
-        return self.__twikipage
-    
+    def setTwikiPage(self, twikipage):
+        self.__twikipage = twikipage
+        
     def exists(datasetName):
         return len(Dataset.search(datasetName)) == 1
     exists = staticmethod(exists)
@@ -69,3 +65,46 @@ class Dataset(object):
         
         return parameters
     createSearchParameters = staticmethod(createSearchParameters)
+    
+    def isValid(self):
+        return Dataset.exists(self.Name) and Dataset.isValidDataflag(self.isData) and Dataset.isValidTwikipage(self.TwikiPage)
+    
+    def isValidDataflag(dataflag):
+        return type(dataflag) is bool
+    isValidDataflag = staticmethod(isValidDataflag)
+    
+    def isValidTwikipage(twikiPage):
+        return twikiPage in Dataset.validTwikis
+    isValidTwikipage = staticmethod(isValidTwikipage)
+    
+    def retrieveProperties(self):
+        if Dataset.exists(self.Name):
+            self.isData = self.__getDataType() == 'data'
+            self.__selectTwikiPage()
+        else:
+            raise IOError('Dataset "%s" does not exist!' % self.Name)
+        
+    def createDatasetFromSQLResult(result):
+        dataset = Dataset(result['name'])
+        dataset.TwikiPage = result['twikiPage']
+        if not result['isData'] is None:
+            dataset.isData = bool(result['isData'])
+        return dataset
+    createDatasetFromSQLResult = staticmethod(createDatasetFromSQLResult)
+    
+    def createDatasetFromUserInput(datasetName):
+        dataset = Dataset(datasetName)
+        dataset.retrieveProperties()
+        return dataset
+    createDatasetFromUserInput = staticmethod(createDatasetFromUserInput)
+    
+    def __cmp__(self, other):
+        if self.Name > other.Name:
+            return 1
+        elif self.Name < other.Name:
+            return -1
+        else:
+            return 0
+        
+    def __eq__(self, other):
+        return self.Name == other.Name and self.TwikiPage == other.TwikiPage and self.isData == other.isData
