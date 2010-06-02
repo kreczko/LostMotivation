dd78e8e206c9e03d2f7b307f02a66e9531188bdf
kreczko <>
2010-06-02 19:23:38 +0000

    added creation from XML content

--- DataFlowMonitoring/src/tests/testGridJob.py
+++ DataFlowMonitoring/src/tests/testGridJob.py
@@ -7,13 +7,32 @@ Email: kreczko@cern.ch
 '''
 import unittest
 from tools.GridJob import GridJob
-
+from xml.dom.minidom import parseString
 class TestGridJob(unittest.TestCase):
     def setUp(self):
         self.gridjob = GridJob()
-        self.gridjob.ID = 33
+        self.gridjob.ID = 2
         self.gridjob.NumberOfRetries = 4
-        self.expectedWildcard = '*_33_4.root'
+        self.expectedWildcard = '*_2_4.root'
+        self.XMLNodeString = """<item>
+            <STATUS>S</STATUS>
+            <resubmissions>4</resubmissions>
+            <EventRange>2</EventRange>
+            <started>2010-06-02 12:05:06</started>
+            <GridEndId>D</GridEndId>
+            <AppGenericStatusReasonValue>Application finished properly
+            </AppGenericStatusReasonValue>
+            <finished>2010-06-02 13:48:21</finished>
+            <submitted>2010-06-02 11:52:03</submitted>
+            <Site>hep.ph.ic.ac.uk</Site>
+            <TaskJobId>164721888</TaskJobId>
+            <JobExecExitCode>0</JobExecExitCode>
+            <SchedulerJobId>https://lb006.cnaf.infn.it:9000/MHgePlRGyLKD_L9q79qcRA
+            </SchedulerJobId>
+            <GridEndReason>Job terminated successfully</GridEndReason>
+        </item>"""
+        self.XMLNode = parseString(self.XMLNodeString)
+        self.XMLgridjob = GridJob.createFromXMLNode(self.XMLNode)
         
     def testStatusSuccess(self):
         self.assertFalse(self.gridjob.isSuccessful())
@@ -43,3 +62,28 @@ class TestGridJob(unittest.TestCase):
     def testGetFileWildcard(self):
         self.assertEquals(self.expectedWildcard, self.gridjob.getFileWildcard())
         
+    def testCreateFromXMLNodeStatus(self):
+        self.assertTrue(self.XMLgridjob.isSuccessful())
+        
+    def testCreateFromXMLNodeNumberOfRetries(self):
+        self.assertEquals(4, self.XMLgridjob.NumberOfRetries)
+        
+    def testCreateFromXMLNodeJobID(self):
+        self.assertEquals(2, self.XMLgridjob.ID)
+        
+    def testCreateFromXMLNodeStartDate(self):
+        self.assertEquals('2010-06-02 12:05:06', self.XMLgridjob.startDate)
+        
+    def testCreateFromXMLNodeFinishDate(self):
+        self.assertEquals('2010-06-02 13:48:21', self.XMLgridjob.finishDate)
+        
+    def testCreateFromXMLNodeSubmissionDate(self):
+        self.assertEquals('2010-06-02 11:52:03', self.XMLgridjob.submissionDate)
+        
+    def testEquals(self):
+        self.assertEquals(self.gridjob, self.XMLgridjob)
+    
+    def testCompare(self):
+        self.gridjob.ID = 44
+        self.assertTrue(self.gridjob > self.XMLgridjob)
+        self.assertTrue(self.XMLgridjob, self.gridjob)
--- DataFlowMonitoring/src/tools/GridJob.py
+++ DataFlowMonitoring/src/tools/GridJob.py
@@ -3,15 +3,18 @@ Created on 2 Jun 2010
 
 @author: kreczko
 '''
+import XMLParser
 
 class GridJob(object):
     basicFileWildcard = '*_%d_%d.root'
+    XMLObjectRoot = 'item'
+    
     def __init__(self):
         self.Status = ''
         self.ID = 0
         self.NumberOfRetries = 0
-        self.submittedDate = ''
-        self.finishedDate = ''
+        self.submissionDate = ''
+        self.finishDate = ''
         self.startDate = ''
         
     def isSuccessful(self):
@@ -31,3 +34,32 @@ class GridJob(object):
     
     def getFileWildcard(self):
         return GridJob.basicFileWildcard % (self.ID, self.NumberOfRetries)
+    
+    def createFromXMLNode(node):
+        idNode = XMLParser.getSubNode(node, 'EventRange')
+        statusNode = XMLParser.getSubNode(node, 'STATUS')
+        numberOfRetriesNode = XMLParser.getSubNode(node, 'resubmissions')
+        submissionDateNode = XMLParser.getSubNode(node, 'submitted')
+        finishDateNode = XMLParser.getSubNode(node, 'finished')
+        startDateNode = XMLParser.getSubNode(node, 'started')
+        
+        gridjob = GridJob()
+        gridjob.ID = int(XMLParser.getText(idNode))
+        gridjob.NumberOfRetries = int(XMLParser.getText(numberOfRetriesNode))
+        gridjob.Status = XMLParser.getText(statusNode)
+        gridjob.submissionDate = XMLParser.getText(submissionDateNode)
+        gridjob.finishDate = XMLParser.getText(finishDateNode)
+        gridjob.startDate = XMLParser.getText(startDateNode)
+        return gridjob
+    createFromXMLNode = staticmethod(createFromXMLNode)
+    
+    def __eq__(self, other):
+        return self.ID == other.ID
+    
+    def __cmp__(self, other):
+        if self.ID > other.ID:
+            return 1
+        elif self.ID < other.ID:
+            return -1
+        else:
+            return 0
