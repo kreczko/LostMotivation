7fa30494ccf6009aa2900ab24a232688ddd74fa5
kreczko <>
2010-07-01 19:17:41 +0000

    added MET, Electron isolation and d0 filter settings

--- BAT_Tests/src/TestFilter.cpp
+++ BAT_Tests/src/TestFilter.cpp
@@ -6,6 +6,7 @@
 #include "Taggers/BJetTagger.h"
 #include "RecoObjects/Jet.h"
 #include "RecoObjects/Electron.h"
+#include "RecoObjects/MET.h"
 
 using namespace BAT;
 static Filter* filter;
@@ -45,7 +46,36 @@ void testSetGoodJetMinimalElectromagneticFraction() {
 void testSetGoodElectronMinimalEt() {
 	setUpTestFilter();
 	ASSERT_EQUAL_DELTA(30.0, Electron::goodElectronMinimalEt, 0.01);
+	filter->setGoodElectronMinimalEt(50.5);
 	ASSERT_EQUAL_DELTA(50.5, Electron::goodElectronMinimalEt, 0.01);
+}
+
+void testSetGoodElectronMaximalAbsoluteEta() {
+	setUpTestFilter();
+	ASSERT_EQUAL_DELTA(2.5, Electron::goodElectronMaximalAbsoluteEta, 0.01);
+	filter->setGoodElectronMaximalAbsoluteEta(1.442);
+	ASSERT_EQUAL_DELTA(1.442, Electron::goodElectronMaximalAbsoluteEta, 0.01);
+}
+
+void testSetGoodElectronMaximalRelativeIsolation() {
+	setUpTestFilter();
+	ASSERT_EQUAL_DELTA(0.1, Electron::isolatedElectronMaximalRelativeIsolation, 0.01);
+	filter->setIsolatedElectronMaximalRelativeIsolation(0.05);
+	ASSERT_EQUAL_DELTA(0.05, Electron::isolatedElectronMaximalRelativeIsolation, 0.005);
+}
+
+void testSetGoodElectronMaximalDistanceFromInteractionPoint() {
+	setUpTestFilter();
+	ASSERT_EQUAL_DELTA(200, Electron::goodElectronMaximalDistanceFromInteractionPoint, 0.1);
+	filter->setGoodElectronMaximalDistanceFromInteractionPoint(100);
+	ASSERT_EQUAL_DELTA(100, Electron::goodElectronMaximalDistanceFromInteractionPoint, 0.1);
+}
+
+void testSetGoodMETMinimalEt() {
+	setUpTestFilter();
+	ASSERT_EQUAL_DELTA(20, MET::goodMETMinimalEt, 0.1);
+	filter->setGoodMETMinimalEt(30);
+	ASSERT_EQUAL_DELTA(30, MET::goodMETMinimalEt, 0.1);
 }
 
 cute::suite make_suite_TestFilter() {
@@ -55,6 +85,10 @@ cute::suite make_suite_TestFilter() {
 	s.push_back(CUTE(testSetGoodJetMaximalAbsoluteEta));
 	s.push_back(CUTE(testSetGoodJetMinimalElectromagneticFraction));
 	s.push_back(CUTE(testSetGoodElectronMinimalEt));
+	s.push_back(CUTE(testSetGoodElectronMaximalAbsoluteEta));
+	s.push_back(CUTE(testSetGoodElectronMaximalRelativeIsolation));
+	s.push_back(CUTE(testSetGoodElectronMaximalDistanceFromInteractionPoint));
+	s.push_back(CUTE(testSetGoodMETMinimalEt));
 	return s;
 }
 
