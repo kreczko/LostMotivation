6c7c8565ef8f95e4c15ff25cb7dc1de184e9315a
kreczko <>
2010-07-01 19:17:00 +0000

    included the ROOTLearningTests again

--- BAT_Tests/src/Test.cpp
+++ BAT_Tests/src/Test.cpp
@@ -5,7 +5,7 @@
 #include "TestEvent.h"
 #include "TestVariableReader.h"
 #include "TestFilter.h"
-//#include "ROOTLearnTests.h"
+#include "ROOTLearnTests.h"
 
 
 //void testStringConstructorThrowsAtIllegalName(){
@@ -19,7 +19,7 @@ void runSuite(){
 	cute::suite s = make_suite_TestEvent();
 	s+= make_suite_TestVariableReader();
 	s+= make_suite_TestFilter();
-//	s+= make_suite_ROOTLearnTests();
+	s+= make_suite_ROOTLearnTests();
 	cute::ide_listener lis;
 	cute::makeRunner(lis)(s, "Testing BAT");
 }
