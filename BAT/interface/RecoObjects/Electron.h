f47d164cb5d09aa6730ac1a8dba7cdeafe84cef8
kreczko <>
2010-07-01 10:20:19 +0000

    implemented 3 of the selection variables and added function stubs

--- BAT/interface/RecoObjects/Electron.h
+++ BAT/interface/RecoObjects/Electron.h
@@ -12,8 +12,15 @@ namespace BAT {
 
 class Electron {
 public:
+	static float goodElectronMinimalEt;
+	static float goodElectronMaximalAbsoluteEta;
+	static float isolatedElectronMaximalRelativeIsolation;
 	Electron();
 	virtual ~Electron();
+	bool isIsolated();
+	bool isInBarrelRegion();
+	bool isInCrack();
+	bool isInEndCapRegion();
 };
 
 }
--- BAT/src/RecoObjects/Electron.cpp
+++ BAT/src/RecoObjects/Electron.cpp
@@ -8,6 +8,9 @@
 #include "../../interface/RecoObjects/Electron.h"
 
 namespace BAT {
+float Electron::goodElectronMinimalEt = 0;
+float Electron::goodElectronMaximalAbsoluteEta = 5;
+float Electron::isolatedElectronMaximalRelativeIsolation = 2;
 
 Electron::Electron() {
 	// TODO Auto-generated constructor stub
