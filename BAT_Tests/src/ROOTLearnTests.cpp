78eabbc639ea6b0a0dc1dda71041a6755116b523
kreczko <>
2010-07-01 19:16:40 +0000

    - removed segfault handler - corrected initialisation of input

--- BAT_Tests/src/ROOTLearnTests.cpp
+++ BAT_Tests/src/ROOTLearnTests.cpp
@@ -15,47 +15,33 @@ static string energyForEachElectron = "els_energy";
 
 static TString invalidBranch("this is not in the chain");
 
-void handler(int sig) throw(string){
-  void *array[10];
-  size_t size;
-
-  // get void*'s for all entries on the stack
-  size = backtrace(array, 10);
-
-  // print out all the frames to stderr
-  fprintf(stderr, "Error: signal %d:\n", sig);
-  backtrace_symbols_fd(array, size, 2);
-  throw "A segmentation fault occured";
-}
-
 void setUpROOTTests() {
 	input = new TChain("configurableAnalysis/eventB");
-	input->Add("/storage/top/mc/summer09_7TeV/MG/HLTskim_ttjet_7TeV_v5/*_1*.root");
+	input->Add("/storage/top/mc/summer09_7TeV/MG/HLTskim_ttjet_7TeV_v5/HLTskim_S9nTuple_ttjet_7TeV_v5_1.root");
 
 	input->GetEntries();
 	input->SetBranchStatus("*", 0);
 	input->SetBranchStatus(numberOfElectrons.c_str(), 1);
 	input->SetBranchStatus(energyForEachElectron.c_str(), 1);
-//	input->GetEntry(1);
+	//	input->GetEntry(1);
 }
 
 void testInvalidTBranch() {
-	signal(SIGSEGV, handler);
-	try{
-		TBranch* br = (TBranch*)input->GetListOfBranches()->FindObject(invalidBranch);
-//		input->SetBranchStatus(invalidBranch.c_str(), 1);
-	}
-	catch(...){
-		cout << "exception occured" << endl;
+	setUpROOTTests();
+	try {
+		TBranch* br = input->GetBranch(invalidBranch);
+		if (br) {
+			input->SetBranchStatus(invalidBranch, 1);
+		}
+	} catch (...) {
+		cout << "exception occurred" << endl;
 	}
 
 }
 
-cute::suite make_suite_ROOTLearnTests(){
+cute::suite make_suite_ROOTLearnTests() {
 	cute::suite s;
 	s.push_back(CUTE(testInvalidTBranch));
 	return s;
 }
-
-
 
