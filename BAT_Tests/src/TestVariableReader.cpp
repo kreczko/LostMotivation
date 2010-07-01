5daf9d42e63ceac60bcb2c706ef420b65ce790c9
kreczko <>
2010-07-01 19:18:36 +0000

    - changed behaviour of non standard constructors - removed static variable check - changed std::string to TString

--- BAT_Tests/src/TestVariableReader.cpp
+++ BAT_Tests/src/TestVariableReader.cpp
@@ -5,42 +5,42 @@
 #include "Readers/VariableReader.h"
 #include <vector>
 #include <string>
-#include <iostream>
-
 #include "TChain.h"
+#include "TString.h"
+#include "TROOT.h"
 
+using namespace ROOT;
 using namespace BAT;
 using namespace std;
 
 static VariableReader<unsigned int>* singleVariableReader;
 static VariableReader<vector<float>*>* multipleVariableReader;
-static string validVariableName = "aValidVariableName";
-static string invalidEmptyVariableName = "";
+static const TString invalidEmptyVariableName("");
+static const TString invalidNotAvailableVariableName("thisIsNotInTheFile");
 static TChain* input;
 
 //select two related variables
-static string numberOfElectrons = "Nels";
-static string energyForEachElectron = "els_energy";
+static const TString numberOfElectrons("Nels");
+static const TString energyForEachElectron("els_energy");
+
+
+void setUpOnce(){
+	gROOT->ProcessLine("#include <vector>");
+}
 
 void setUpVariableReaders() {
 	input = new TChain("configurableAnalysis/eventB");
-	input->Add("/storage/top/mc/summer09_7TeV/MG/HLTskim_ttjet_7TeV_v5/*_1*.root");
+	input->Add("/storage/top/mc/summer09_7TeV/MG/HLTskim_ttjet_7TeV_v5/*_1.root");
 
 	singleVariableReader = new VariableReader<unsigned int>::VariableReader(input, numberOfElectrons);
 	multipleVariableReader = new VariableReader<std::vector<float>*>::VariableReader(input, energyForEachElectron);
 	input->GetEntries();
 	input->SetBranchStatus("*", 0);
-	input->SetBranchStatus(numberOfElectrons.c_str(), 1);
-	input->SetBranchStatus(energyForEachElectron.c_str(), 1);
+	input->SetBranchStatus(numberOfElectrons, 1);
+	input->SetBranchStatus(energyForEachElectron, 1);
 	input->GetEntry(1);
 }
 
-void testIsValidVariableName() {
-	ASSERT(VariableReader<>::isValidVariableName(validVariableName));
-}
-void testInvalidEmptyVariableName() {
-	ASSERT(!VariableReader<>::isValidVariableName(invalidEmptyVariableName));
-}
 void testReadSingleVariable(){
 	setUpVariableReaders();
 	ASSERT_EQUAL(3, singleVariableReader->getVariable());
@@ -49,17 +49,27 @@ void testReadSingleVariable(){
 void testReadMultipleVariable(){
 	setUpVariableReaders();
 	ASSERT_EQUAL_DELTA(193.008, multipleVariableReader->getVariable()->at(0), 0.001);
-	ASSERT_EQUAL(singleVariableReader->getVariable(), multipleVariableReader->getVariable()->size());
+//	ASSERT_EQUAL(singleVariableReader->getVariable(), multipleVariableReader->getVariable()->size());
+}
+
+void testInvalidVariableThrowsException(){
+	setUpVariableReaders();
+	ASSERT_THROWS(new VariableReader<int>::VariableReader(input, invalidNotAvailableVariableName), VariableNotFoundException);
+}
+void testInvalidEmptyVariableThrowsException(){
+	setUpVariableReaders();
+	ASSERT_THROWS(new VariableReader<int>::VariableReader(input, invalidEmptyVariableName), VariableNotFoundException);
 }
 
 
 
 cute::suite make_suite_TestVariableReader() {
+	setUpOnce();
 	cute::suite s;
-	s.push_back(CUTE(testIsValidVariableName));
-	s.push_back(CUTE(testInvalidEmptyVariableName));
 	s.push_back(CUTE(testReadSingleVariable));
 	s.push_back(CUTE(testReadMultipleVariable));
+	s.push_back(CUTE(testInvalidVariableThrowsException));
+	s.push_back(CUTE(testInvalidEmptyVariableThrowsException));
 	return s;
 }
 
