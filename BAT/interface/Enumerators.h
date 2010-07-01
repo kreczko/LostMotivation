2bf7a890a85d2156056a2e54961fda6aee17c3cc
kreczko <>
2010-07-01 19:13:14 +0000

    moved most of the definitions to the corresponding classes It is to be seen if this header file is needed at all

--- BAT/interface/Enumerators.h
+++ BAT/interface/Enumerators.h
@@ -8,50 +8,7 @@
 #ifndef ENUMERATORS_H_
 #define ENUMERATORS_H_
 namespace BAT {
-namespace BTag{
 
-enum Algorithm {
-	NONE,
-	FAKE,
-	TrackCountingHighEfficiency,
-	TrackCountingHighPurity,
-	JetBProbability,
-	JetProbability,
-	SimpleSecondaryVertex,
-	SoftElectron,
-	SoftMuon,
-	SoftMuonNoIP,
-	NUMBER_OF_BTAGALGORITHMS
-};
-}
-
-namespace Jet{
-enum Algorithm{
-	DEFAULT,
-	JPT_AntiKt_ConeDR05,
-	KT_Cone04,
-	KT_Cone06,
-	ParticleFlow,
-	SiS_Cone05,
-	SiS_Cone07,
-	NUMBER_OF_JETALGORITHMS
-};
-}
-
-namespace MET{
-enum Type{
-	DEFAULT,
-	CalorimeterMET_mujes,
-	SC5,
-	SC7,
-	KT4,
-	KT6,
-	tcMET,
-	ParticleFlowMET,
-	MCTruth,
-	NUMBER_OF_METALGORITHMS
-};
-}
 
 enum SelectionStages{
 	NUMBER_OF_SELECTIONSTAGES
