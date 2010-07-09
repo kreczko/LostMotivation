6b2a9d74b2e58696890ee3f97ccfe5837abc0a93
kreczko <>
2010-07-09 15:57:38 +0000

    simplified ObjectReader might delete it completely

--- BAT/interface/Readers/ObjectReader.h
+++ BAT/interface/Readers/ObjectReader.h
@@ -7,13 +7,20 @@
 
 #ifndef OBJECTREADER_H_
 #define OBJECTREADER_H_
+#include "TChain.h"
+#include <boost/shared_ptr.hpp>
 
 namespace BAT {
 
 class ObjectReader {
 public:
 	ObjectReader();
+	ObjectReader(TChain* input);
 	virtual ~ObjectReader();
+
+protected:
+	boost::shared_ptr<TChain> input;
+	virtual void readObject();
 };
 
 }
