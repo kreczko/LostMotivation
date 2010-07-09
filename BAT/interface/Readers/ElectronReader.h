a01fe22ff9c1d2101f4b496f56eff3872de8f941
kreczko <>
2010-07-09 17:08:31 +0000

    added first variables to ElectronReader

--- BAT/interface/Readers/ElectronReader.h
+++ BAT/interface/Readers/ElectronReader.h
@@ -7,13 +7,28 @@
 
 #ifndef ELECTRONREADER_H_
 #define ELECTRONREADER_H_
+#include "../RecoObjects/Electron.h"
+#include "VariableReader.h"
+#include <vector>
+#include "TChain.h"
 
 namespace BAT {
-
-class ElectronReader {
+class ElectronReader{
 public:
 	ElectronReader();
+	ElectronReader(TChain* input);
 	virtual ~ElectronReader();
+	std::vector<Electron> getElectrons();
+private:
+	VariableReader<unsigned int> numberOfElectronsReader;
+	VariableReader<MultiFloatPointer> energyReader;
+	VariableReader<MultiFloatPointer> pxReader;
+	VariableReader<MultiFloatPointer> pyReader;
+	VariableReader<MultiFloatPointer> pzReader;
+
+	std::vector<Electron> electrons;
+	void readElectrons();
+
 };
 
 }
--- BAT/src/Readers/ElectronReader.cpp
+++ BAT/src/Readers/ElectronReader.cpp
@@ -6,11 +6,15 @@
  */
 
 #include "../../interface/Readers/ElectronReader.h"
-
 namespace BAT {
 
-ElectronReader::ElectronReader() {
-	// TODO Auto-generated constructor stub
+ElectronReader::ElectronReader() :
+	numberOfElectronsReader(), energyReader(), pxReader(), pyReader(), pzReader(), electrons() {
+
+}
+ElectronReader::ElectronReader(TChain* input) :
+	numberOfElectronsReader(input, "Nels"), energyReader(input, "els_energy"), pxReader(input, "els_px"), pyReader(
+			input, "els_py"), pzReader(input, "els_pz"), electrons() {
 
 }
 
@@ -18,4 +22,23 @@ ElectronReader::~ElectronReader() {
 	// TODO Auto-generated destructor stub
 }
 
+std::vector<Electron> ElectronReader::getElectrons() {
+	if (electrons.empty())
+		readElectrons();
+	return electrons;
 }
+
+void ElectronReader::readElectrons() {
+	unsigned int numberOfElectrons = numberOfElectronsReader.getVariable();
+	for (unsigned int electronIndex = 0; electronIndex < numberOfElectrons; electronIndex++) {
+		float energy = energyReader.getVariable()->at(electronIndex);
+		float px = pxReader.getVariable()->at(electronIndex);
+		float py = pyReader.getVariable()->at(electronIndex);
+		float pz = pzReader.getVariable()->at(electronIndex);
+		Electron electron(energy, px, py, pz);
+		electrons.push_back(electron);
+	}
+}
+
+}
+
