9fa8049894dfc124e596bcd1ed4f38c8c084328b
kreczko <>
2010-07-09 17:09:51 +0000

    corrected mistake: disabled all branches before the initialisation of the readers.

--- BAT_Tests/src/TestVariableReader.cpp
+++ BAT_Tests/src/TestVariableReader.cpp
@@ -18,50 +18,47 @@ static VariableReader<vector<float>*>* multipleVariableReader;
 static const TString invalidEmptyVariableName("");
 static const TString invalidNotAvailableVariableName("thisIsNotInTheFile");
 static TChain* input;
+static TChain* nullChain(0);
 
 //select two related variables
 static const TString numberOfElectrons("Nels");
 static const TString energyForEachElectron("els_energy");
 
-
-void setUpOnce(){
+void setUpOnce() {
 	gROOT->ProcessLine("#include <vector>");
 }
 
 void setUpVariableReaders() {
 	input = new TChain("configurableAnalysis/eventB");
 	input->Add("/storage/top/mc/summer09_7TeV/MG/HLTskim_ttjet_7TeV_v5/*_1.root");
-
-	singleVariableReader = new VariableReader<unsigned int>::VariableReader(input, numberOfElectrons);
-	multipleVariableReader = new VariableReader<std::vector<float>*>::VariableReader(input, energyForEachElectron);
 	input->GetEntries();
 	input->SetBranchStatus("*", 0);
-	input->SetBranchStatus(numberOfElectrons, 1);
-	input->SetBranchStatus(energyForEachElectron, 1);
+	singleVariableReader = new VariableReader<unsigned int>::VariableReader(input, numberOfElectrons);
+	multipleVariableReader = new VariableReader<std::vector<float>*>::VariableReader(input, energyForEachElectron);
 	input->GetEntry(1);
 }
 
-void testReadSingleVariable(){
+void testReadSingleVariable() {
 	setUpVariableReaders();
 	ASSERT_EQUAL(3, singleVariableReader->getVariable());
 }
 
-void testReadMultipleVariable(){
+void testReadMultipleVariable() {
 	setUpVariableReaders();
 	ASSERT_EQUAL_DELTA(193.008, multipleVariableReader->getVariable()->at(0), 0.001);
-//	ASSERT_EQUAL(singleVariableReader->getVariable(), multipleVariableReader->getVariable()->size());
+	ASSERT_EQUAL(singleVariableReader->getVariable(), multipleVariableReader->getVariable()->size());
 }
 
-void testInvalidVariableThrowsException(){
+void testInvalidVariableThrowsException() {
 	setUpVariableReaders();
 	ASSERT_THROWS(new VariableReader<int>::VariableReader(input, invalidNotAvailableVariableName), VariableNotFoundException);
 }
-void testInvalidEmptyVariableThrowsException(){
+
+
+void testInvalidEmptyVariableThrowsException() {
 	setUpVariableReaders();
 	ASSERT_THROWS(new VariableReader<int>::VariableReader(input, invalidEmptyVariableName), VariableNotFoundException);
 }
-
-
 
 cute::suite make_suite_TestVariableReader() {
 	setUpOnce();
