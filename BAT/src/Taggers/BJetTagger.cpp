384224ea0526c45455b351246b6bfb8c5a3278d2
kreczko <>
2010-07-14 00:21:01 +0000

    - removed unecessary comments - added initialisation of variable

--- BAT/src/Taggers/BJetTagger.cpp
+++ BAT/src/Taggers/BJetTagger.cpp
@@ -9,21 +9,4 @@
 
 namespace BAT {
 float BJetTagger::simpleSecondaryVertexHighEfficiencyMediumCut = -1000;
-//static bool BJetTagger::tag(Btag::Algorithm type, float discriminator) {
-//	switch (type) {
-//	case BTag::NONE:
-//		return true;
-//	default:
-//		break;
-//	}
-//}
 }
-//BJetTagger::BJetTagger() {
-//	// TODO Auto-generated constructor stub
-//
-//}
-//
-//BJetTagger::~BJetTagger() {
-//	// TODO Auto-generated destructor stub
-//}
-
