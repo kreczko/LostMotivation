6b6f1441a5d21f9e4e916794363dcec8c5a125aa
kreczko <>
2010-07-14 00:30:22 +0000

    done a lot ;)

--- BAT/interface/Readers/NTupleEventReader.h
+++ BAT/interface/Readers/NTupleEventReader.h
@@ -7,26 +7,36 @@
 
 #ifndef NTUPLEEVENTREADER_H_
 #define NTUPLEEVENTREADER_H_
-#include "../Event.h"
 #include <boost/scoped_ptr.hpp>
+#include <boost/shared_ptr.hpp>
 #include "TChain.h"
+#include "../Event.h"
+#include "ElectronReader.h"
 
 namespace BAT {
 
 class NTupleEventReader {
+	static const char * EVENT_CHAIN;
+	static const char * HLT_TRIGGER_CHAIN;
 public:
 	NTupleEventReader();
 	virtual ~NTupleEventReader();
-	unsigned int getNumberOfEvents();
+	unsigned long getNumberOfEvents();
 	Event* getNextEvent();
+	void addInputFile(const char* fileName);
+	void addInputFile(const char* fileName, Event::DataType type);
 private:
+	unsigned long numberOfEvents;
 	unsigned long currentEventEntry;
-	boost::scoped_ptr<TChain> input;
-	boost::scoped_ptr<TChain> hltInput;
-	void constructEvent();
+	unsigned long numberOfFiles;
+	boost::shared_ptr<TChain> input;
+	boost::shared_ptr<TChain> hltTriggerInput;
+	boost::scoped_ptr<ElectronReader> electronReader;
+	bool areReadersSet;
 	void selectNextNtupleEvent();
+	void initateNumberOfEventsIfNotSet();
+	void initiateReadersIfNotSet();
 };
-
 }
 
 #endif /* NTUPLEEVENTREADER_H_ */
