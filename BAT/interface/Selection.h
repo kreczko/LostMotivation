3e304a0a1c321989bdf3cc3589c9661df0b077dd
kreczko <>
2010-07-19 12:24:30 +0000

    initial import of the definitions of the SelectionSteps

---
+++ BAT/interface/Selection.h
@@ -0,0 +1,14 @@
+namespace BAT {
+namespace Selection {
+enum SelectionSteps {
+	HighLevelTrigger,
+	GoodPrimaryvertex,
+	OneIsolatedElectron,
+	ConversionRejection,
+	LooseMuonVeto,
+	AtLeastFourGoodJets,
+	Zveto,
+	NUMBER_OF_SELECTION_STEPS
+};
+}
+}
