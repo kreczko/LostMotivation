0470ffdcd9ea7049bd79ca24edd1978930e47de6
kreczko <>
2010-07-19 12:32:09 +0000

    added stub for the ParticleReader

---
+++ BAT/interface/Readers/ParticleReader.h
@@ -0,0 +1,21 @@
+/*
+ * ParticleReader.h
+ *
+ *  Created on: 18 Jul 2010
+ *      Author: kreczko
+ */
+
+#ifndef PARTICLEREADER_H_
+#define PARTICLEREADER_H_
+
+namespace BAT {
+
+class ParticleReader {
+public:
+	ParticleReader();
+	virtual ~ParticleReader();
+};
+
+}
+
+#endif /* PARTICLEREADER_H_ */
---
+++ BAT/src/Readers/ParticleReader.cpp
@@ -0,0 +1,21 @@
+/*
+ * ParticleReader.cpp
+ *
+ *  Created on: 18 Jul 2010
+ *      Author: kreczko
+ */
+
+#include "../../interface/Readers/ParticleReader.h"
+
+namespace BAT {
+
+ParticleReader::ParticleReader() {
+	// TODO Auto-generated constructor stub
+
+}
+
+ParticleReader::~ParticleReader() {
+	// TODO Auto-generated destructor stub
+}
+
+}
