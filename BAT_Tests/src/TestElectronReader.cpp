af63b474975ea92eb132532d272016587865adce
kreczko <>
2010-07-19 20:07:40 +0000

    - added tests of isolation - added tearDown function to prevent memory leaks

--- BAT_Tests/src/TestElectronReader.cpp
+++ BAT_Tests/src/TestElectronReader.cpp
@@ -21,25 +21,47 @@ void setUpElectronReader() {
 	input->GetEntry(1);
 }
 
-
+void tearDownElectronReader(){
+	delete electronReader;
+}
 void testReadElectronsSize() {
 	setUpElectronReader();
 	std::vector<Electron> electrons = electronReader->getElectrons();
 	ASSERT_EQUAL(6, electrons.size());
+	tearDownElectronReader();
 }
 
-void testReadFirstElectronEnergy(){
+void testReadFirstElectronEnergy() {
+	setUpElectronReader();
 	std::vector<Electron> electrons = electronReader->getElectrons();
 	Electron firstElectron = electrons.front();
 	ASSERT_EQUAL_DELTA(38.7786, firstElectron.energy(), 0.0001);
+	tearDownElectronReader();
 }
 
+void testReadFirstElectronIsIsolated() {
+	setUpElectronReader();
+	std::vector<Electron> electrons = electronReader->getElectrons();
+	Electron firstElectron = electrons.front();
+	ASSERT(firstElectron.isIsolated());
+	tearDownElectronReader();
+}
 
+void testReadFirstElectronIsIsolatedAfterChangingCut() {
+	setUpElectronReader();
+	Electron::isolatedElectronMaximalRelativeIsolation = 0.00001;
+	std::vector<Electron> electrons = electronReader->getElectrons();
+	Electron firstElectron = electrons.front();
+	ASSERT(firstElectron.isIsolated() == false);
+	tearDownElectronReader();
+}
 
 cute::suite make_suite_TestElectronReader() {
 	cute::suite s;
 	s.push_back(CUTE(testReadElectronsSize));
 	s.push_back(CUTE(testReadFirstElectronEnergy));
+	s.push_back(CUTE(testReadFirstElectronIsIsolated));
+	s.push_back(CUTE(testReadFirstElectronIsIsolatedAfterChangingCut));
 	return s;
 }
 
