23dc5cfe781bebd2653dfc2c7934b2f1b8263348
kreczko <>
2010-07-19 12:10:47 +0000

    - sorted commits and executions by name - included tests for NTupleEventReader, JetReader and Jet

--- BAT_Tests/src/Test.cpp
+++ BAT_Tests/src/Test.cpp
@@ -2,31 +2,30 @@
 #include "ide_listener.h"
 #include "cute_runner.h"
 
-#include "TestEvent.h"
-#include "TestVariableReader.h"
-#include "TestFilter.h"
 #include "ROOTLearnTests.h"
-#include "TestParticle.h"
 #include "TestElectron.h"
-#include "TestEventCounter.h"
 #include "TestElectronReader.h"
-
-//void testStringConstructorThrowsAtIllegalName(){
-//	ASSERT_THROWS(new BAT::Reader(""), std::);
-//}
-
-
-
+#include "TestEvent.h"
+#include "TestEventCounter.h"
+#include "TestFilter.h"
+#include "TestJet.h"
+#include "TestJetReader.h"
+#include "TestNTupleReader.h"
+#include "TestParticle.h"
+#include "TestVariableReader.h"
 
 void runSuite(){
-	cute::suite s = make_suite_TestEvent();
-	s+= make_suite_TestVariableReader();
-	s+= make_suite_TestFilter();
-	s+= make_suite_ROOTLearnTests();
-	s+= make_suite_TestParticle();
+	cute::suite s = make_suite_ROOTLearnTests();
 	s+= make_suite_TestElectron();
-	s+= make_suite_TestEventCounter();
 	s+= make_suite_TestElectronReader();
+	s+= make_suite_TestEvent();
+	s+= make_suite_TestEventCounter();
+	s+= make_suite_TestFilter();
+	s+= make_suite_TestJet();
+	s+= make_suite_TestJetReader();
+	s+= make_suite_TestNTupleReader();
+	s+= make_suite_TestParticle();
+	s+= make_suite_TestVariableReader();
 	cute::ide_listener lis;
 	cute::makeRunner(lis)(s, "Testing BAT");
 }
