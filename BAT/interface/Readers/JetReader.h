fb3471b84a5ce14116242688279450a460a7544b
kreczko <>
2010-07-19 12:26:17 +0000

    first implementation of the jet reader

--- BAT/interface/Readers/JetReader.h
+++ BAT/interface/Readers/JetReader.h
@@ -7,13 +7,37 @@
 
 #ifndef JETREADER_H_
 #define JETREADER_H_
+#include "../RecoObjects/Jet.h"
+#include "VariableReader.h"
+#include <vector>
+#include "TChain.h"
+#include <boost/shared_ptr.hpp>
 
 namespace BAT {
 
 class JetReader {
 public:
 	JetReader();
+	JetReader(boost::shared_ptr<TChain> input);
+	JetReader(boost::shared_ptr<TChain> input, Jet::Algorithm algorithm);
 	virtual ~JetReader();
+	std::vector<Jet> getJets();
+	void initialise();
+private:
+	VariableReader<unsigned int> numberOfJetsReader;
+	VariableReader<MultiFloatPointer> energyReader;
+	VariableReader<MultiFloatPointer> pxReader;
+	VariableReader<MultiFloatPointer> pyReader;
+	VariableReader<MultiFloatPointer> pzReader;
+
+	VariableReader<MultiFloatPointer> emfReader;
+
+	VariableReader<MultiFloatPointer> btagSimpleSecondaryVertexReader;
+	VariableReader<MultiFloatPointer> btagTrackCountingHighPurityReader;
+	VariableReader<MultiFloatPointer> btagTrackCountingHighEfficiencyReader;
+
+	std::vector<Jet> jets;
+	void readJets();
 };
 
 }
--- BAT/src/Readers/JetReader.cpp
+++ BAT/src/Readers/JetReader.cpp
@@ -9,11 +9,56 @@
 
 namespace BAT {
 
-JetReader::JetReader() {
+JetReader::JetReader() :
+	numberOfJetsReader(), energyReader(), pxReader(), pyReader(), pzReader(), emfReader(),
+			btagSimpleSecondaryVertexReader(), btagTrackCountingHighPurityReader(),
+			btagTrackCountingHighEfficiencyReader(), jets() {
 
 }
+JetReader::JetReader(boost::shared_ptr<TChain> input) :
+	numberOfJetsReader(input, "Njets"), energyReader(input, "jets_energy"), pxReader(input, "jets_px"), pyReader(input,
+			"jets_py"), pzReader(input, "jets_pz"), emfReader(input, "jets_emf"), btagSimpleSecondaryVertexReader(
+			input, "jets_btag_secVertex"), btagTrackCountingHighPurityReader(input, "jets_btag_TC_highPur"),
+			btagTrackCountingHighEfficiencyReader(input, "jets_btag_TC_highEff"), jets() {
 
+}
 JetReader::~JetReader() {
 }
 
+std::vector<Jet> JetReader::getJets() {
+	if (jets.empty() == false)
+		jets.clear();
+	readJets();
+	return jets;
+}
+
+void JetReader::readJets() {
+	unsigned int numberOfJets = numberOfJetsReader.getVariable();
+	for (unsigned int jetIndex = 0; jetIndex < numberOfJets; jetIndex++) {
+		float energy = energyReader.getVariable()->at(jetIndex);
+		float px = pxReader.getVariable()->at(jetIndex);
+		float py = pyReader.getVariable()->at(jetIndex);
+		float pz = pzReader.getVariable()->at(jetIndex);
+		Jet jet(energy, px, py, pz);
+		jet.setEMF(emfReader.getVariable()->at(jetIndex));
+		jet.setDiscriminatorForBtagType(btagSimpleSecondaryVertexReader.getVariable()->at(jetIndex),
+				BJetTagger::SimpleSecondaryVertex);
+		jet.setDiscriminatorForBtagType(btagTrackCountingHighPurityReader.getVariable()->at(jetIndex),
+				BJetTagger::TrackCountingHighPurity);
+		jet.setDiscriminatorForBtagType(btagTrackCountingHighEfficiencyReader.getVariable()->at(jetIndex),
+				BJetTagger::TrackCountingHighEfficiency);
+		jets.push_back(jet);
+	}
+}
+void JetReader::initialise() {
+	numberOfJetsReader.initialise();
+	energyReader.initialise();
+	pxReader.initialise();
+	pyReader.initialise();
+	pzReader.initialise();
+	emfReader.initialise();
+	btagSimpleSecondaryVertexReader.initialise();
+	btagTrackCountingHighPurityReader.initialise();
+	btagTrackCountingHighEfficiencyReader.initialise();
+}
 }
