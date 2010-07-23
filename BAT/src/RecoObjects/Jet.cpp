e6cb8b0d6f17d7b7742c528d365a3dc10d8dd9a7
kreczko <>
2010-07-23 16:10:10 +0000

    - added getters and setters for emf, n90Hits and fHPD and isGood()

--- BAT/src/RecoObjects/Jet.cpp
+++ BAT/src/RecoObjects/Jet.cpp
@@ -15,10 +15,16 @@ float Jet::goodJetMinimalNumberOfRecHitsContaining90PercentOfTheJetEnergy = 0;
 float Jet::goodJetMaximalFractionOfEnergyIntheHottestHPDReadout = 5000.;
 
 Jet::Jet() :
-	Particle(), electroMagneticFraction(0.), btag_discriminators(BJetTagger::NUMBER_OF_BTAGALGORITHMS) {
+	Particle(), electroMagneticFraction(0.), numberOfRecHitsContaining90PercentOfTheJetEnergy(0.),
+			fractionOfEnergyIntheHottestHPDReadout(0.), btag_discriminators(BJetTagger::NUMBER_OF_BTAGALGORITHMS) {
 
 }
 
+Jet::Jet(const Particle& particle) :
+	Particle(particle), electroMagneticFraction(0.), numberOfRecHitsContaining90PercentOfTheJetEnergy(0.),
+			fractionOfEnergyIntheHottestHPDReadout(0.), btag_discriminators(BJetTagger::NUMBER_OF_BTAGALGORITHMS) {
+
+}
 Jet::Jet(float energy, float px, float py, float pz) :
 	Particle(energy, px, py, pz), electroMagneticFraction(0.),
 			btag_discriminators(BJetTagger::NUMBER_OF_BTAGALGORITHMS) {
@@ -27,15 +33,40 @@ Jet::Jet(float energy, float px, float py, float pz) :
 
 Jet::~Jet() {
 }
-float Jet::emf() {
+float Jet::emf() const {
 	return electroMagneticFraction;
+}
+
+float Jet::n90Hits() const {
+	return numberOfRecHitsContaining90PercentOfTheJetEnergy;
+}
+
+float Jet::fHPD() const {
+	return fractionOfEnergyIntheHottestHPDReadout;
 }
 
 void Jet::setEMF(float emf) {
 	electroMagneticFraction = emf;
 }
 
+void Jet::setN90Hits(float n90Hits) {
+	numberOfRecHitsContaining90PercentOfTheJetEnergy = n90Hits;
+}
+
+void Jet::setFHPD(float fHPD) {
+	fractionOfEnergyIntheHottestHPDReadout = fHPD;
+}
+
 void Jet::setDiscriminatorForBtagType(float discriminator, BJetTagger::Algorithm type) {
 	btag_discriminators[type] = discriminator;
+}
+
+bool Jet::isGoodJet() const{
+	bool passesEt = et() > Jet::goodJetMinimalEt;
+	bool passesEta = fabs(eta()) < Jet::goodJetMaximalAbsoluteEta;
+	bool passesEMF = emf() > Jet::goodJetMinimalElectromagneticFraction;
+	bool passesN90Hits = n90Hits() > Jet::goodJetMinimalNumberOfRecHitsContaining90PercentOfTheJetEnergy;
+	bool passesFHPD = fHPD() < Jet::goodJetMaximalFractionOfEnergyIntheHottestHPDReadout;
+	return passesEt && passesEta && passesEMF && passesN90Hits && passesFHPD;
 }
 }
