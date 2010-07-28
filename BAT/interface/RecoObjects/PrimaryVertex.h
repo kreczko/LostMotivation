b32b2f9c683bc439ea858b378b38b50f2e0cb0a3
kreczko <>
2010-07-28 18:21:05 +0000

    initial import of Primary vertex class

---
+++ BAT/interface/RecoObjects/PrimaryVertex.h
@@ -0,0 +1,46 @@
+/*
+ * PrimaryVertex.h
+ *
+ *  Created on: 25 Jul 2010
+ *      Author: kreczko
+ */
+
+#ifndef PRIMARYVERTEX_H_
+#define PRIMARYVERTEX_H_
+
+namespace BAT {
+
+class PrimaryVertex {
+public:
+	static unsigned int goodVertexMinimalNumberOfDegreesOfFreedom;
+	static float goodVertexMaximalAbsoluteZPosition;
+	static float goodVertexMaximalAbsoluteRho;
+
+	static void resetSelectionValues() {
+		PrimaryVertex::goodVertexMinimalNumberOfDegreesOfFreedom = 0;
+		PrimaryVertex::goodVertexMaximalAbsoluteZPosition = 5000;
+		PrimaryVertex::goodVertexMaximalAbsoluteRho = 5000;
+	}
+
+	PrimaryVertex();
+	virtual ~PrimaryVertex();
+	bool isGood() const;
+	bool isFake() const;
+	unsigned int ndof() const;
+	float absoluteZPosition() const;
+	float absoluteRho() const;
+	void setFake(bool fake);
+	void setDegreesOfFreedom(unsigned int ndof);
+	void setZPosition(float z);
+	void setRho(float rho);
+private:
+	bool is_fake;
+	unsigned int degreesOfFreedom;
+	float z_position;
+	float rho;
+
+};
+
+}
+
+#endif /* PRIMARYVERTEX_H_ */
---
+++ BAT/src/RecoObjects/PrimaryVertex.cpp
@@ -0,0 +1,62 @@
+/*
+ * PrimaryVertex.cpp
+ *
+ *  Created on: 25 Jul 2010
+ *      Author: kreczko
+ */
+
+#include "../../interface/RecoObjects/PrimaryVertex.h"
+#include <math.h>
+namespace BAT {
+unsigned int PrimaryVertex::goodVertexMinimalNumberOfDegreesOfFreedom = 0;
+float PrimaryVertex::goodVertexMaximalAbsoluteZPosition = 5000;
+float PrimaryVertex::goodVertexMaximalAbsoluteRho = 5000;
+
+PrimaryVertex::PrimaryVertex() :
+	is_fake(false), degreesOfFreedom(0), z_position(0), rho(0) {
+
+}
+
+PrimaryVertex::~PrimaryVertex() {
+}
+
+void PrimaryVertex::setDegreesOfFreedom(unsigned int ndof) {
+	degreesOfFreedom = ndof;
+}
+
+void PrimaryVertex::setZPosition(float z) {
+	z_position = z;
+}
+
+void PrimaryVertex::setRho(float rho) {
+	this->rho = rho;
+}
+
+void PrimaryVertex::setFake(bool fake) {
+	is_fake = fake;
+}
+
+unsigned int PrimaryVertex::ndof() const {
+	return degreesOfFreedom;
+}
+
+float PrimaryVertex::absoluteZPosition() const {
+	return fabs(z_position);
+}
+
+float PrimaryVertex::absoluteRho() const {
+	return fabs(rho);
+}
+
+bool PrimaryVertex::isFake() const {
+	return is_fake;
+}
+
+bool PrimaryVertex::isGood() const {
+	bool passesNDOF = ndof() >= PrimaryVertex::goodVertexMinimalNumberOfDegreesOfFreedom;
+	bool passesZ = absoluteZPosition() <= PrimaryVertex::goodVertexMaximalAbsoluteZPosition;
+	bool passesRho = absoluteRho() <= PrimaryVertex::goodVertexMaximalAbsoluteRho;
+	bool isNotFake = isFake() == false;
+	return passesNDOF && passesZ && passesRho && isNotFake;
+}
+}
