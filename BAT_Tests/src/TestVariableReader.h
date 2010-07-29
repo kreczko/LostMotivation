97e942fd68ac8a63ac3f100ad1575914d60088e5
kreczko <>
2010-07-29 16:20:48 +0000

    transformed test for VariableReader into a test fixture

--- BAT_Tests/src/TestVariableReader.h
+++ BAT_Tests/src/TestVariableReader.h
@@ -7,79 +7,59 @@
 #include "TChain.h"
 #include "TString.h"
 
-
 using namespace ROOT;
 using namespace BAT;
 using namespace std;
 
-static const TString invalidEmptyVariableName("");
-static const TString invalidNotAvailableVariableName("thisIsNotInTheFile");
-//select two related variables
-static const TString numberOfElectrons("Nels");
-static const TString energyForEachElectron("els_energy");
+struct TestVariableReader {
+private:
+	TString invalidEmptyVariableName, invalidNotAvailableVariableName, numberOfElectrons, energyForEachElectron;
+	TChainPointer input;
+	boost::scoped_ptr<VariableReader<unsigned int> > singleVariableReader;
+	boost::scoped_ptr<VariableReader<MultiFloatPointer> > multipleVariableReader;
+	boost::scoped_ptr<VariableReader<int> > invalidEmptyVariableVariableReader, invalidnNotAvailableVariableReader;
+public:
+	TestVariableReader() :
+		invalidEmptyVariableName(""), invalidNotAvailableVariableName("thisIsNotInTheFile"), numberOfElectrons("Nels"),
+				energyForEachElectron("els_energy"), input(new TChain("configurableAnalysis/eventB")),
+				singleVariableReader(new VariableReader<unsigned int>::VariableReader(input, numberOfElectrons)),
+				multipleVariableReader(new VariableReader<MultiFloatPointer>::VariableReader(input,
+						energyForEachElectron)), invalidEmptyVariableVariableReader(
+						new VariableReader<int>::VariableReader(input, invalidEmptyVariableName)),
+				invalidnNotAvailableVariableReader(new VariableReader<int>::VariableReader(input,
+						invalidNotAvailableVariableName)) {
+		input->Add("/storage/top/mc/V4/MG/e20skim_ttjet/*_1.root");
+		input->SetBranchStatus("*", 0);
+		singleVariableReader->initialise();
+		multipleVariableReader->initialise();
+		input->LoadTree(1);
+		input->GetEntry(1);
+	}
+	void testReadSingleVariable() {
+		ASSERT_EQUAL(6, singleVariableReader->getVariable());
+	}
 
-static boost::shared_ptr<TChain> input;
+	void testReadMultipleVariable() {
+		ASSERT_EQUAL_DELTA(38.7786, multipleVariableReader->getVariableAt(0), 0.0001);
+		ASSERT_EQUAL(singleVariableReader->getVariable(), multipleVariableReader->size());
+	}
 
-static VariableReader<unsigned int>* singleVariableReader;
-static VariableReader<vector<float>*>* multipleVariableReader;
-static VariableReader<int>* invalidEmptyVariableVariableReader;
-static VariableReader<int>* invalidnNotAvailableVariableReader;
+	void testInvalidVariableThrowsException() {
+		ASSERT_THROWS(invalidnNotAvailableVariableReader->initialise(), VariableNotFoundException);
+	}
 
+	void testInvalidEmptyVariableThrowsException() {
+		ASSERT_THROWS(invalidEmptyVariableVariableReader->initialise(), VariableNotFoundException);
+	}
 
-void setUpVariableReaders() {
-	input = boost::shared_ptr<TChain>(new TChain("configurableAnalysis/eventB"));
-	input->Add("/storage/top/mc/V4/MG/e20skim_ttjet/*_1.root");
-	input->GetEntries();
-	input->SetBranchStatus("*", 0);
-	singleVariableReader = new VariableReader<unsigned int>::VariableReader(input, numberOfElectrons);
-	multipleVariableReader = new VariableReader<std::vector<float>*>::VariableReader(input, energyForEachElectron);
-	invalidEmptyVariableVariableReader = new VariableReader<int>::VariableReader(input, invalidEmptyVariableName);
-	invalidnNotAvailableVariableReader
-			= new VariableReader<int>::VariableReader(input, invalidNotAvailableVariableName);
-	singleVariableReader->initialise();
-	multipleVariableReader->initialise();
-	input->GetEntry(1);
-}
-
-void tearDownVariableReaders(){
-	delete singleVariableReader;
-	delete multipleVariableReader;
-	delete invalidEmptyVariableVariableReader;
-	delete invalidnNotAvailableVariableReader;
-}
-
-void testReadSingleVariable() {
-	setUpVariableReaders();
-	ASSERT_EQUAL(6, singleVariableReader->getVariable());
-	tearDownVariableReaders();
-}
-
-void testReadMultipleVariable() {
-	setUpVariableReaders();
-	ASSERT_EQUAL_DELTA(38.7786, multipleVariableReader->getVariableAt(0), 0.0001);
-	ASSERT_EQUAL(singleVariableReader->getVariable(), multipleVariableReader->size());
-	tearDownVariableReaders();
-}
-
-void testInvalidVariableThrowsException() {
-	setUpVariableReaders();
-	ASSERT_THROWS(invalidnNotAvailableVariableReader->initialise(), VariableNotFoundException);
-	tearDownVariableReaders();
-}
-
-void testInvalidEmptyVariableThrowsException() {
-	setUpVariableReaders();
-	ASSERT_THROWS(invalidEmptyVariableVariableReader->initialise(), VariableNotFoundException);
-	tearDownVariableReaders();
-}
+};
 
 cute::suite make_suite_TestVariableReader() {
-//	setUpOnce();
 	cute::suite s;
-	s.push_back(CUTE(testReadSingleVariable));
-	s.push_back(CUTE(testReadMultipleVariable));
-	s.push_back(CUTE(testInvalidVariableThrowsException));
-	s.push_back(CUTE(testInvalidEmptyVariableThrowsException));
+	s.push_back(CUTE_SMEMFUN(TestVariableReader,testReadSingleVariable));
+	s.push_back(CUTE_SMEMFUN(TestVariableReader,testReadMultipleVariable));
+	s.push_back(CUTE_SMEMFUN(TestVariableReader,testInvalidVariableThrowsException));
+	s.push_back(CUTE_SMEMFUN(TestVariableReader,testInvalidEmptyVariableThrowsException));
 	return s;
 }
 
