941908464fe05a5e0971063ea373444646d39cb0
kreczko <>
2010-07-29 13:50:14 +0000

    - replaced pointer with smart pointer - changed file to run over data

--- BAT/src/Run.cc
+++ BAT/src/Run.cc
@@ -10,22 +10,24 @@
 #include "TROOT.h"
 #include "Analysis.h"
 #include <iostream>
+#include <boost/scoped_ptr.hpp>
 using namespace ROOT;
 using namespace std;
 int main(int argc, char **argv) {
 	gROOT->ProcessLine("#include <vector>");
 	TStopwatch watch;
 	watch.Start();
-	Analysis* myAnalysis = new Analysis();
+	boost::scoped_ptr<Analysis> myAnalysis(new Analysis());
 
 
 	cout << "adding files" << endl;
 	//Input files
-	myAnalysis->addInputFile("/storage/top/mc/V4/MG/e20skim_ttjet/e20skim_nTuple_ttjet_f_1.root");
+	myAnalysis->addInputFile("/storage/top/data/200710/EG_Run2010A-PromptReco-v4_RECO_137437_139558/*.root");
 
 	cout << "starting analysis" << endl;
 	myAnalysis->analyze();
 	watch.Stop();
 	watch.Print();
+
 	return 0;
 }
