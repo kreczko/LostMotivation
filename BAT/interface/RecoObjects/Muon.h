df25bdfcc8d848cc20dce32f722ec0320eac40f7
kreczko <>
2010-07-31 16:46:53 +0000

    added implementation for Muon (initial import)

--- BAT/interface/RecoObjects/Muon.h
+++ BAT/interface/RecoObjects/Muon.h
@@ -8,15 +8,42 @@
 #ifndef MUON_H_
 #define MUON_H_
 #include "Particle.h"
+#include <vector>
 
 namespace BAT {
 
-class Muon : public Particle{
+class Muon: public Particle {
 public:
+	static float goodMuonMinimalPt;
+	static float goodMuonMaximalAbsoluteEta;
+	static float isolatedMuonMaximalRelativeIsolation;
+
+	static void resetSelectionValues() {
+		Muon::goodMuonMinimalPt = 0;
+		Muon::goodMuonMaximalAbsoluteEta = 5;
+		Muon::isolatedMuonMaximalRelativeIsolation = 5000;
+	}
 	Muon();
+	Muon(float energy, float px, float py, float pz);
 	virtual ~Muon();
+	bool isGood() const;
+	bool isIsolated() const;
+	bool isGlobal() const;
+	float ecalIsolation() const;
+	float hcalIsolation() const;
+	float trackerIsolation() const;
+	float relativeIsolation() const;
+
+	void makeGlobal(bool global);
+	void setEcalIsolation(float isolation);
+	void setHcalIsolation(float isolation);
+	void setTrackerIsolation(float isolation);
+private:
+	bool is_Global;
+	float ecal_Isolation, hcal_Isolation, tracker_Isolation;
 };
 
+typedef std::vector<Muon> MuonCollection;
 }
 
 #endif /* MUON_H_ */
--- BAT/src/RecoObjects/Muon.cpp
+++ BAT/src/RecoObjects/Muon.cpp
@@ -8,14 +8,66 @@
 #include "../../interface/RecoObjects/Muon.h"
 
 namespace BAT {
+float Muon::goodMuonMinimalPt = 0;
+float Muon::goodMuonMaximalAbsoluteEta = 5;
+float Muon::isolatedMuonMaximalRelativeIsolation = 5000;
 
-Muon::Muon() {
-	// TODO Auto-generated constructor stub
+Muon::Muon() :
+	Particle(), is_Global(false), ecal_Isolation(0.), hcal_Isolation(0), tracker_Isolation(0) {
+
+}
+
+Muon::Muon(float energy, float px, float py, float pz) :
+	Particle(energy, px, py, pz), is_Global(false), ecal_Isolation(0.), hcal_Isolation(0), tracker_Isolation(0) {
 
 }
 
 Muon::~Muon() {
-	// TODO Auto-generated destructor stub
 }
 
+bool Muon::isGlobal() const {
+	return is_Global;
+}
+
+void Muon::makeGlobal(bool global) {
+	is_Global = global;
+}
+
+float Muon::ecalIsolation() const {
+	return ecal_Isolation;
+}
+
+void Muon::setEcalIsolation(float isolation) {
+	ecal_Isolation = isolation;
+}
+
+float Muon::hcalIsolation() const {
+	return hcal_Isolation;
+}
+
+void Muon::setHcalIsolation(float isolation) {
+	hcal_Isolation = isolation;
+}
+
+float Muon::trackerIsolation() const {
+	return tracker_Isolation;
+}
+
+void Muon::setTrackerIsolation(float isolation) {
+	tracker_Isolation = isolation;
+}
+
+float Muon::relativeIsolation() const {
+	return (ecal_Isolation + hcal_Isolation + tracker_Isolation) / pt();
+}
+
+bool Muon::isIsolated() const{
+	return relativeIsolation() < Muon::isolatedMuonMaximalRelativeIsolation;
+}
+
+bool Muon::isGood() const{
+	bool passesPt = pt() > Muon::goodMuonMinimalPt;
+	bool passesEta = fabs(eta()) < Muon::goodMuonMaximalAbsoluteEta;
+	return passesPt && passesEta && is_Global;
+}
 }
