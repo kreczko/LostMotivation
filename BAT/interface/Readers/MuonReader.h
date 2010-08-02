99dd86253771227cae33757acf59549d1677157e
kreczko <>
2010-08-02 02:55:39 +0000

    added implementation for MuonReader

--- BAT/interface/Readers/MuonReader.h
+++ BAT/interface/Readers/MuonReader.h
@@ -7,13 +7,34 @@
 
 #ifndef MUONREADER_H_
 #define MUONREADER_H_
+#include "VariableReader.h"
+#include "../RecoObjects/Muon.h"
 
 namespace BAT {
 
 class MuonReader {
 public:
 	MuonReader();
+	MuonReader(TChainPointer input);
 	virtual ~MuonReader();
+	const MuonCollection& getMuons();
+	void initialise();
+
+private:
+	VariableReader<unsigned int> numberOfMuonsReader;
+	VariableReader<MultiFloatPointer> energyReader;
+	VariableReader<MultiFloatPointer> pxReader;
+	VariableReader<MultiFloatPointer> pyReader;
+	VariableReader<MultiFloatPointer> pzReader;
+
+	VariableReader<MultiFloatPointer> ecalIsolationReader;
+	VariableReader<MultiFloatPointer> hcalIsolationReader;
+	VariableReader<MultiFloatPointer> trackerIsolationReader;
+
+	VariableReader<MultiFloatPointer> isGlobalReader;
+
+	MuonCollection muons;
+	void readMuons();
 };
 
 }
--- BAT/src/Readers/MuonReader.cpp
+++ BAT/src/Readers/MuonReader.cpp
@@ -9,13 +9,57 @@
 
 namespace BAT {
 
-MuonReader::MuonReader() {
-	// TODO Auto-generated constructor stub
+MuonReader::MuonReader() :
+	numberOfMuonsReader(), energyReader(), pxReader(), pyReader(), pzReader(), ecalIsolationReader(),
+			hcalIsolationReader(), trackerIsolationReader(), isGlobalReader() {
 
 }
 
+MuonReader::MuonReader(TChainPointer input) :
+	numberOfMuonsReader(input, "Nmus"), energyReader(input, "mus_energy"), pxReader(input, "mus_px"), pyReader(input,
+			"mus_py"), pzReader(input, "mus_pz"), ecalIsolationReader(input, "mus_iso03_emEt"), hcalIsolationReader(
+			input, "mus_iso03_hadEt"), trackerIsolationReader(input, "mus_iso03_sumPt"), isGlobalReader(input,
+			"mus_id_AllGlobalMuons") {
+
+}
+
+void MuonReader::initialise() {
+	numberOfMuonsReader.initialise();
+	energyReader.initialise();
+	pxReader.initialise();
+	pyReader.initialise();
+	pzReader.initialise();
+
+	ecalIsolationReader.initialise();
+	hcalIsolationReader.initialise();
+	trackerIsolationReader.initialise();
+
+	isGlobalReader.initialise();
+}
+
+const MuonCollection& MuonReader::getMuons() {
+	if (muons.empty() == false)
+		muons.clear();
+	readMuons();
+	return muons;
+}
+
+void MuonReader::readMuons(){
+	unsigned int numberOfMuons = numberOfMuonsReader.getVariable();
+		for (unsigned int index = 0; index < numberOfMuons; index++) {
+			float energy = energyReader.getVariableAt(index);
+			float px = pxReader.getVariableAt(index);
+			float py = pyReader.getVariableAt(index);
+			float pz = pzReader.getVariableAt(index);
+			Muon muon(energy, px, py, pz);
+			muon.setEcalIsolation(ecalIsolationReader.getVariableAt(index));
+			muon.setHcalIsolation(hcalIsolationReader.getVariableAt(index));
+			muon.setTrackerIsolation(trackerIsolationReader.getVariableAt(index));
+			muon.makeGlobal(isGlobalReader.getVariableAt(index) == 1);
+			muons.push_back(muon);
+		}
+}
 MuonReader::~MuonReader() {
-	// TODO Auto-generated destructor stub
 }
 
 }
