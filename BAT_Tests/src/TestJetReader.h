f03b34bd72679d34b3724322b295afdceb7455c7
kreczko <>
2010-08-02 03:05:00 +0000

    promoted variables jets and firstJet to struct variables

--- BAT_Tests/src/TestJetReader.h
+++ BAT_Tests/src/TestJetReader.h
@@ -9,46 +9,37 @@ struct TestJetReader {
 private:
 	boost::shared_ptr<TChain> input;
 	boost::scoped_ptr<JetReader> reader;
+	JetCollection jets;
+	Jet firstJet;
 
 public:
 	TestJetReader() :
-		input(new TChain("configurableAnalysis/eventB")), reader(new JetReader(input)) {
+		input(new TChain("configurableAnalysis/eventB")), reader(new JetReader(input)), jets(), firstJet() {
 		input->Add("/storage/top/mc/V4/MG/e20skim_ttjet/e20skim_nTuple_ttjet_f_1.root");
 		input->SetBranchStatus("*", 0);
-		//	numberOfJetsReader = new VariableReader<unsigned int>(input, "Njets");//left here to demonstrate that the normal way doesn't work
 		reader->initialise();
-		//	numberOfJetsReader->initialise();//left here to demonstrate that the normal way doesn't work
 		input->GetEntry(1);
+		jets = reader->getJets();
+		firstJet = jets.front();
 	}
 
 	void testReadJetsSize() {
-		std::vector<Jet> jets = reader->getJets();
-		//	reading the same variable two times produces a segmentation fault!
-		//	ASSERT_EQUAL(numberOfJetsReader->getVariable(), jets.size());//left here to demonstrate that the normal way doesn't work
 		ASSERT_EQUAL(8, jets.size());
 	}
 
 	void testReadFirstJetEnergy() {
-		std::vector<Jet> jets = reader->getJets();
-		Jet firstJet = jets.front();
 		ASSERT_EQUAL_DELTA(210.836, firstJet.energy(), 0.001);
 	}
 
 	void testReadFirstJetEMF() {
-		std::vector<Jet> jets = reader->getJets();
-		Jet firstJet = jets.front();
 		ASSERT_EQUAL_DELTA(0.436829, firstJet.emf(), 0.00001);
 	}
 
 	void testReadFirstJetn90Hits() {
-		std::vector<Jet> jets = reader->getJets();
-		Jet firstJet = jets.front();
 		ASSERT_EQUAL_DELTA(136, firstJet.n90Hits(), 0.1);
 	}
 
 	void testReadFirstJetfHPD() {
-		std::vector<Jet> jets = reader->getJets();
-		Jet firstJet = jets.front();
 		ASSERT_EQUAL_DELTA(0.378509, firstJet.fHPD(), 0.00001);
 	}
 };
