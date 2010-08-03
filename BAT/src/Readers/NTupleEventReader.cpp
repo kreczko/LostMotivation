246364239e4d3f45273bfed2bb2fd12e49938801
kreczko <>
2010-08-03 10:55:52 +0000

    - added ECAL spikes cleaning chain - added HLT reader - corrected behaviour of selectNextEntry & hasNextEvent: hasNextEvent HAS to be called before the initialisation of the readers (Friend TChain issue)!

--- BAT/src/Readers/NTupleEventReader.cpp
+++ BAT/src/Readers/NTupleEventReader.cpp
@@ -7,10 +7,13 @@
 
 #include "../../interface/Readers/NTupleEventReader.h"
 #include "TFile.h"
+#include <iostream>
+using namespace std;
 namespace BAT {
 
 const char * NTupleEventReader::EVENT_CHAIN = "configurableAnalysis/eventB";
-const char * NTupleEventReader::HLT_TRIGGER_CHAIN = "configurableAnalysis/eventB";
+const char * NTupleEventReader::HLT_TRIGGER_CHAIN = "configurableAnalysis/eventV";
+const char * NTupleEventReader::ECAL_SPIKE_CLEANING_CHAIN = "configurableAnalysis/eventA";
 
 const boost::array<std::string, 12> NTupleEventReader::FileTypes = { { "ttbar", "ttjet", "tchan", "tW", "wj", "zj",
 		"bce1", "bce2", "bce3", "enri1", "enri2", "enri3" } };
@@ -20,11 +23,15 @@ const std::string NTupleEventReader::FilePrefix = "nTuple_";
 NTupleEventReader::NTupleEventReader() :
 	processedEvents(0), maximalNumberOfEvents(999999999), currentEventEntry(0), numberOfFiles(0), input(new TChain(
 			NTupleEventReader::EVENT_CHAIN)), hltTriggerInput(new TChain(NTupleEventReader::HLT_TRIGGER_CHAIN)),
-			electronReader(new ElectronReader(input)), jetReader(new JetReader(input)), muonReader(
-					new MuonReader(input)), runNumberReader(new VariableReader<unsigned int> (input, "run")),
+			ecalSpikeCleaningInput(new TChain(NTupleEventReader::ECAL_SPIKE_CLEANING_CHAIN)), HLTReader(
+					new VariableReader<double> (hltTriggerInput, "HLT_Photon15_L1R")), primaryReader(
+					new PrimaryVertexReader(input)), electronReader(new ElectronReader(input)), jetReader(
+					new JetReader(input)), muonReader(new MuonReader(input)), runNumberReader(new VariableReader<
+					unsigned int> (input, "run")),
 			eventNumberReader(new VariableReader<unsigned int> (input, "event")), lumiBlockReader(new VariableReader<
 					unsigned int> (input, "lumiblock")), areReadersSet(false), currentEvent() {
 	input->AddFriend(hltTriggerInput.get());
+	input->AddFriend(ecalSpikeCleaningInput.get());
 }
 
 NTupleEventReader::~NTupleEventReader() {
@@ -33,12 +40,14 @@ NTupleEventReader::~NTupleEventReader() {
 void NTupleEventReader::addInputFile(const char * fileName) {
 	numberOfFiles += input->Add(fileName);
 	hltTriggerInput->Add(fileName);
+	ecalSpikeCleaningInput->Add(fileName);
 }
 
 const Event& NTupleEventReader::getNextEvent() {
-	initiateReadersIfNotSet();
 	selectNextNtupleEvent();
 	currentEvent = Event();
+	currentEvent.setHLT_Photon15_L1R(HLTReader->getVariable() > 0);
+	currentEvent.setPrimaryVertex(primaryReader->getVertex());
 	currentEvent.setElectrons(electronReader->getElectrons());
 	currentEvent.setJets(jetReader->getJets());
 	currentEvent.setMuons(muonReader->getMuons());
@@ -52,9 +61,33 @@ const Event& NTupleEventReader::getNextEvent() {
 
 void NTupleEventReader::selectNextNtupleEvent() {
 	if (hasNextEvent()) {
+		initiateReadersIfNotSet();
 		input->GetEntry(currentEventEntry);
 		currentEventEntry++;
 		processedEvents++;
+	}
+}
+
+bool NTupleEventReader::hasNextEvent() {
+	bool hasNextInNTuple = input->LoadTree(currentEventEntry) >= 0;
+	bool isWithinEventRange = currentEventEntry < maximalNumberOfEvents;
+	return hasNextInNTuple && isWithinEventRange;
+}
+
+void NTupleEventReader::initiateReadersIfNotSet() {
+	if (areReadersSet == false) {
+		input->SetBranchStatus("*", 0);
+		hltTriggerInput->SetBranchStatus("*", 0);
+		ecalSpikeCleaningInput->SetBranchStatus("*", 0);
+		HLTReader->initialise();
+		primaryReader->initialise();
+		electronReader->initialise();
+		jetReader->initialise();
+		muonReader->initialise();
+		runNumberReader->initialise();
+		eventNumberReader->initialise();
+		lumiBlockReader->initialise();
+		areReadersSet = true;
 	}
 }
 
@@ -98,26 +131,6 @@ std::string NTupleEventReader::findCurrentFileType() {
 		}
 	}
 	return filetype;
-}
-
-void NTupleEventReader::initiateReadersIfNotSet() {
-	if (areReadersSet == false) {
-		input->SetBranchStatus("*", 0);
-		hltTriggerInput->SetBranchStatus("*", 0);
-		electronReader->initialise();
-		jetReader->initialise();
-		muonReader->initialise();
-		runNumberReader->initialise();
-		eventNumberReader->initialise();
-		lumiBlockReader->initialise();
-		areReadersSet = true;
-	}
-}
-
-bool NTupleEventReader::hasNextEvent() {
-	bool hasNextInNTuple = input->LoadTree(currentEventEntry) >= 0;
-	bool isWithinEventRange = currentEventEntry < maximalNumberOfEvents;
-	return hasNextInNTuple && isWithinEventRange;
 }
 
 unsigned long NTupleEventReader::getNumberOfProccessedEvents() const {
