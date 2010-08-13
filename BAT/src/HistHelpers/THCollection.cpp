fa099a68f91112239e429fa0f69bd60256b95e64
kreczko <>
2010-08-13 16:10:35 +0000

    added implementation of add for TH2 histograms

--- BAT/src/HistHelpers/THCollection.cpp
+++ BAT/src/HistHelpers/THCollection.cpp
@@ -9,7 +9,14 @@
 
 namespace BAT {
 template<>
-void THCollection<TH1>::add(std::string name, std::string title, unsigned int numberOfBins, double xmin, double xmax){
+void THCollection<TH1>::add(std::string name, std::string title, unsigned int numberOfBins, double xmin, double xmax) {
     histMap[name] = boost::shared_ptr<TH1>(new TH1D(name.c_str(), title.c_str(), numberOfBins, xmin, xmax));
+}
+
+template<>
+void THCollection<TH2>::add(std::string name, std::string title, unsigned int numberOfXBins, double xmin, double xmax,
+        unsigned int numberOfYBins, double ymin, double ymax) {
+    histMap[name] = boost::shared_ptr<TH2>(new TH2D(name.c_str(), title.c_str(), numberOfXBins, xmin, xmax,
+            numberOfYBins, ymin, ymax));
 }
 }
