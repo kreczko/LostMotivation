24ce8110a56da0541dd6bf12a7509e1f97068a09
kreczko <>
2010-08-22 19:53:29 +0000

    added the event printer Class

---
+++ BAT/interface/Printers/EventPrinter.h
@@ -0,0 +1,17 @@
+/*
+ * EventPrinter.h
+ *
+ *  Created on: 22 Aug 2010
+ *      Author: kreczko
+ */
+
+#ifndef EVENTPRINTER_H_
+#define EVENTPRINTER_H_
+
+class EventPrinter {
+public:
+    EventPrinter();
+    virtual ~EventPrinter();
+};
+
+#endif /* EVENTPRINTER_H_ */
---
+++ BAT/src/Printers/EventPrinter.cpp
@@ -0,0 +1,17 @@
+/*
+ * EventPrinter.cpp
+ *
+ *  Created on: 22 Aug 2010
+ *      Author: kreczko
+ */
+
+#include "../../interface/Printers/EventPrinter.h"
+
+EventPrinter::EventPrinter() {
+    // TODO Auto-generated constructor stub
+
+}
+
+EventPrinter::~EventPrinter() {
+    // TODO Auto-generated destructor stub
+}
