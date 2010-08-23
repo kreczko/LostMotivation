9103dea8757be83974913aa93ce7a25dc7fc8020
kreczko <>
2010-08-23 17:31:20 +0000

    added VBTF95 constants

--- BAT/interface/VBTF_ElectronID.h
+++ BAT/interface/VBTF_ElectronID.h
@@ -20,6 +20,18 @@ const float MaximalDPhiIn_EndcapRegion = 0.02;
 const float MaximalDEtaIn_EndcapRegion = 0.005;
 const float MaximalHadOverEm_EndcapRegion = 0.025;
 }
+
+namespace VBTF_W95 {
+const float MaximalSigmaIEtaIEta_BarrelRegion = 0.01;
+const float MaximalDPhiIn_BarrelRegion = 0.8;
+const float MaximalDEtaIn_BarrelRegion = 0.007;
+const float MaximalHadOverEm_BarrelRegion = 0.15;
+
+const float MaximalSigmaIEtaIEta_EndcapRegion = 0.03;
+const float MaximalDPhiIn_EndcapRegion = 0.7;
+const float MaximalDEtaIn_EndcapRegion = 0.01;
+const float MaximalHadOverEm_EndcapRegion = 0.07;
+}
 }
 
 #endif /* VBTF_ELECTRONID_H_ */
