7ea12f35e018da64a5e7afddbb1f387e5f439876
kreczko <>
2010-08-23 18:19:30 +0000

    added definitions for vector<double> VariableReader
    needs clean up!

--- BAT/interface/Readers/VariableReader.h
+++ BAT/interface/Readers/VariableReader.h
@@ -14,6 +14,8 @@
 #include <exception>
 
 typedef std::vector<float>* MultiFloatPointer;
+typedef std::vector<double>* MultiDoublePointer;
+
 typedef boost::shared_ptr<TChain> TChainPointer;
 namespace BAT {
 struct VariableNotFoundException: public std::exception {
@@ -113,6 +115,57 @@ public:
 private:
     boost::shared_ptr<TChain> input;
     MultiFloatPointer variable;
+    TString variableName;
+
+    void readVariableFromInput() {
+        input->SetBranchAddress(variableName, &variable);
+    }
+
+    void enableVariable() {
+        input->SetBranchStatus(variableName, true);
+    }
+};
+
+
+template<>
+class VariableReader<MultiDoublePointer> {
+public:
+    VariableReader() :
+        input(), variable(0), variableName("") {
+
+    }
+
+    VariableReader(boost::shared_ptr<TChain> chain, TString varName) :
+        input(chain), variable(0), variableName(varName) {
+
+    }
+
+    ~VariableReader() {
+        delete variable;
+    }
+
+    unsigned int size() const {
+        return variable->size();
+    }
+
+    double getVariableAt(unsigned int index) {
+        return variable->at(index);
+    }
+
+    void initialise() {
+        if (doesVariableExist()) {
+            enableVariable();
+            readVariableFromInput();
+        } else
+            throw VariableNotFoundException("Variable '" + variableName + "' was not found.");
+    }
+
+    bool doesVariableExist() {
+        return input->GetBranch(variableName) != NULL;
+    }
+private:
+    boost::shared_ptr<TChain> input;
+    MultiDoublePointer variable;
     TString variableName;
 
     void readVariableFromInput() {
