b172beee19cefd05cf5233895e95c08f47e089f0
kreczko <>
2010-09-21 21:20:29 +0000

    - added scraping filter - swaped order of Z veto and >= 4 jet cut

--- BAT/interface/Selection.h
+++ BAT/interface/Selection.h
@@ -5,18 +5,26 @@
 namespace BAT {
 namespace TTbarEPlusJetsSelection {
 enum Step {
-	HighLevelTrigger,
-	GoodPrimaryvertex,
-	OneIsolatedElectron,
-	ConversionRejection,
-	LooseMuonVeto,
-	AtLeastFourGoodJets,
-	Zveto,
-	NUMBER_OF_SELECTION_STEPS
+    FilterOutScraping,
+    HighLevelTrigger,
+    GoodPrimaryvertex,
+    OneIsolatedElectron,
+    ConversionRejection,
+    LooseMuonVeto,
+    Zveto,
+    AtLeastFourGoodJets,
+    NUMBER_OF_SELECTION_STEPS
 };
 
-const std::string StringSteps[NUMBER_OF_SELECTION_STEPS] = { "High Level Trigger", "good primary vertex",
-		"exactly one isolated electron", "Conversion veto", "loose muon veto", ">= 4 jets", "Z veto" };
+const std::string StringSteps[NUMBER_OF_SELECTION_STEPS] = {
+        "Scraping Filter",
+        "High Level Trigger",
+        "good primary vertex",
+        "exactly one isolated electron",
+        "Conversion veto",
+        "loose muon veto",
+        "Z veto",
+        ">= 4 jets" };
 }
 }
 #endif //SELECTION_H_
