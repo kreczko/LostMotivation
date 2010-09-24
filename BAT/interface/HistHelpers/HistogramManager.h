7ac3e28c7edd1380ff8db97198531f107aa9f9ee
kreczko <>
2010-09-24 19:31:55 +0000

    - implemented operators [] and () for the HistogramManager class. This will be used to access the histograms.

--- BAT/interface/HistHelpers/HistogramManager.h
+++ BAT/interface/HistHelpers/HistogramManager.h
@@ -8,8 +8,8 @@
 #ifndef HISTOGRAMMANAGER_H_
 #define HISTOGRAMMANAGER_H_
 #include "THCollection.h"
-#include "TH1F.h"
-#include "TH2F.h"
+#include "TH1D.h"
+#include "TH2D.h"
 #include "../Taggers/BJetTagger.h"
 #include "../../interface/Enumerators.h"
 #include <boost/shared_ptr.hpp>
@@ -22,15 +22,17 @@ public:
 	virtual ~HistogramManager();
 	void createAllHistograms();
 
-	void fillHistogram(ushort histogram, const float value, const double weight);
-	void fill2DHistograms(ushort histogram, const float Xvalue, const float Yvalue, const double weight);
-	void fillLevelBinnedHistogram(const ushort histogram, ushort level, const float value, const float weight);
-	void fillLevelBinnedHistogram(const ushort hist, ushort level, const float valueX, const float valueY,
-			const float weight);
-	void fillNjetBinnedHistogram(ushort histogram, const float value, const double w);
-	void fillHisto_btag_DataAndMC(BJetTagger::Algorithm btagAlgorithm, ushort histogram, const float value, const double w);
+//	void fillHistogram(ushort histogram, const float value, const double weight);
+//	void fill2DHistograms(ushort histogram, const float Xvalue, const float Yvalue, const double weight);
+//	void fillLevelBinnedHistogram(const ushort histogram, ushort level, const float value, const float weight);
+//	void fillLevelBinnedHistogram(const ushort hist, ushort level, const float valueX, const float valueY,
+//			const float weight);
+//	void fillNjetBinnedHistogram(ushort histogram, const float value, const double w);
+//	void fillHisto_btag_DataAndMC(BJetTagger::Algorithm btagAlgorithm, ushort histogram, const float value, const double w);
 	void setCurrentDataType(DataType::value type);
 	void prepairForSeenDataTypes(const boost::array<bool, DataType::NUMBER_OF_DATA_TYPES>& seenDataTypes);
+	boost::shared_ptr<TH1> operator[](std::string);
+	boost::shared_ptr<TH2> operator()(std::string);
 private:
 	TH1Collection collection;
 	TH2Collection collection2D;
--- BAT/src/HistHelpers/HistogramManager.cpp
+++ BAT/src/HistHelpers/HistogramManager.cpp
@@ -21,4 +21,12 @@ void HistogramManager::setCurrentDataType(DataType::value type) {
     currentDataType = type;
 }
 
+boost::shared_ptr<TH1> HistogramManager::operator [](std::string histname) {
+    return collection.get(histname);
+}
+
+boost::shared_ptr<TH2> HistogramManager::operator ()(std::string histname) {
+    return collection2D.get(histname);
+}
+
 }
