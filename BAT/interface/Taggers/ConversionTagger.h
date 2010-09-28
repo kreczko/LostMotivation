3658d84714b948582b9760dfa00370b514d86cf0
kreczko <>
2010-09-28 17:28:43 +0000

    added implementation of the ConversionFinder in CMSSW (not the same as Frankie's)

--- BAT/interface/Taggers/ConversionTagger.h
+++ BAT/interface/Taggers/ConversionTagger.h
@@ -9,15 +9,29 @@
 #define CONVERSIONTAGGER_H_
 #include "../RecoObjects/Electron.h"
 #include "../RecoObjects/Track.h"
+#include <boost/shared_ptr.hpp>
 
 namespace BAT {
 
 class ConversionTagger {
+private:
+    ElectronPointer electron;
+    TrackCollection tracks;
+    TrackPointer partnerTrack;
+    double minFracSharedHits;
+    double distance, deltaCot;
+    int electronTrackID;
+    void findClosestTrack(const TrackPointer electronTrack);
 public:
-	ConversionTagger();
-	virtual ~ConversionTagger();
-	void calculateConversionVariables(const ElectronPointer electron, const TrackCollection tracks);
+    ConversionTagger();
+    virtual ~ConversionTagger();
+    void calculateConversionVariables(const ElectronPointer electron, const TrackCollection tracks,
+            const double bfieldAtOrigin, double minFracSharedHits);
+    const TrackPointer getElectronTrack();
+    bool isFromConversion(double maxAbsDistance, double maxAbsDeltaCot);
 };
+
+typedef boost::shared_ptr<ConversionTagger> ConversionTaggerPointer;
 
 }
 
