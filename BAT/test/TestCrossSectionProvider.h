c542c3b98035a80c6f196114e26acb9b519d95d9
kreczko <>
2010-10-12 15:10:47 +0000

    changed tests for CrossSectionProvider to accomodate for the changes within

--- BAT/test/TestCrossSectionProvider.h
+++ BAT/test/TestCrossSectionProvider.h
@@ -14,11 +14,12 @@ using namespace BAT;
 
 struct TestCrossSectionProvider {
 private:
-    CrossSectionProvider xSectionProvider;
     float lumi;
+    CrossSectionProvider xSectionProvider;
+
 public:
     TestCrossSectionProvider() :
-        xSectionProvider(), lumi(20) {
+        lumi(20), xSectionProvider(lumi) {
         xSectionProvider.useSkimEfficiency(false);
     }
 
@@ -26,85 +27,95 @@ public:
 
     }
 
-    void testGetExpectedTTbarEventsFor20pb() {
-        ASSERT_EQUAL(157.5*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::ttbar, lumi));
+    void testGetExpectedTTbarEvents() {
+        ASSERT_EQUAL(157.5*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::ttbar));
     }
 
-    void testGetExpectedWjetsEventsFor20pb() {
-        ASSERT_EQUAL(31314.*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::Wjets, lumi));
+    void testGetExpectedWjetsEvents() {
+        ASSERT_EQUAL(31314.*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::Wjets));
     }
 
-    void testGetExpectedZjetsEventsFor20pb() {
-        ASSERT_EQUAL(3048.*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::Zjets, lumi));
+    void testGetExpectedZjetsEvents() {
+        ASSERT_EQUAL(3048.*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::Zjets));
     }
 
-    void testGetExpectedQCD_EMEnriched_Pt20to30EventsFor20pb() {
-        ASSERT_EQUAL(0.2355e9 * 0.0073*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_EMEnriched_Pt20to30, lumi));
+    void testGetExpectedQCD_EMEnriched_Pt20to30Events() {
+        ASSERT_EQUAL(0.2355e9 * 0.0073*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_EMEnriched_Pt20to30));
     }
 
-    void testGetExpectedQCD_EMEnriched_Pt30to80EventsFor20pb() {
-        ASSERT_EQUAL(0.0593e9 * 0.059*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_EMEnriched_Pt30to80, lumi));
+    void testGetExpectedQCD_EMEnriched_Pt30to80Events() {
+        ASSERT_EQUAL(0.0593e9 * 0.059*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_EMEnriched_Pt30to80));
     }
 
-    void testGetExpectedQCD_EMEnriched_Pt80to170EventsFor20pb() {
-        ASSERT_EQUAL(0.906e6 * 0.148*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_EMEnriched_Pt80to170, lumi));
+    void testGetExpectedQCD_EMEnriched_Pt80to170Events() {
+        ASSERT_EQUAL(0.906e6 * 0.148*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_EMEnriched_Pt80to170));
     }
 
-    void testGetExpectedQCD_BCtoE_Pt20to30EventsFor20pb() {
-        ASSERT_EQUAL(0.2355e9 * 0.00046*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_BCtoE_Pt20to30, lumi));
+    void testGetExpectedQCD_BCtoE_Pt20to30Events() {
+        ASSERT_EQUAL(0.2355e9 * 0.00046*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_BCtoE_Pt20to30));
     }
 
-    void testGetExpectedQCD_BCtoE_Pt30to80EventsFor20pb() {
-        ASSERT_EQUAL(0.0593e9 * 0.00234*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_BCtoE_Pt30to80, lumi));
+    void testGetExpectedQCD_BCtoE_Pt30to80Events() {
+        ASSERT_EQUAL(0.0593e9 * 0.00234*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_BCtoE_Pt30to80));
     }
 
-    void testGetExpectedQCD_BCtoE_Pt80to170EventsFor20pb() {
-        ASSERT_EQUAL(0.906e6 * 0.0104*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_BCtoE_Pt80to170, lumi));
+    void testGetExpectedQCD_BCtoE_Pt80to170Events() {
+        ASSERT_EQUAL(0.906e6 * 0.0104*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::QCD_BCtoE_Pt80to170));
     }
 
-    void testGetExpectedVQQEventsFor20pb() {
-        ASSERT_EQUAL(36.*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::VQQ, lumi));
+    void testGetExpectedVQQEvents() {
+        ASSERT_EQUAL(36.*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::VQQ));
     }
 
-    void testGetExpectedsingleTop_And_WEventsFor20pb() {
-        ASSERT_EQUAL(10.6*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::singleTop_And_W, lumi));
+    void testGetExpectedsingleTop_And_WEvents() {
+        ASSERT_EQUAL(10.6*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::singleTop_And_W));
     }
 
-    void testGetExpectedsingleTopTChannelEventsFor20pb() {
-        ASSERT_EQUAL_DELTA(21.53*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::singleTopTChannel, lumi), 0.1);
+    void testGetExpectedsingleTopTChannelEvents() {
+        ASSERT_EQUAL_DELTA(21.53*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::singleTopTChannel), 0.1);
     }
 
-    void testGetExpectedsingleTopSChannelEventsFor20pb() {
-        ASSERT_EQUAL(1.40*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::singleTopSChannel, lumi));
+    void testGetExpectedsingleTopSChannelEvents() {
+        ASSERT_EQUAL(1.40*lumi, xSectionProvider.getExpectedNumberOfEvents(DataType::singleTopSChannel));
     }
 
-    void testGetExpectedDataEventsFor20pb() {
-        ASSERT_EQUAL(0, xSectionProvider.getExpectedNumberOfEvents(DataType::DATA, lumi));
+    void testGetExpectedDataEvents() {
+        ASSERT_EQUAL(0, xSectionProvider.getExpectedNumberOfEvents(DataType::DATA));
+    }
+
+    void testGetWeightData() {
+        ASSERT_EQUAL(1, xSectionProvider.getWeight(DataType::DATA));
+    }
+
+    void testGetWeightTtbar() {
+        ASSERT_EQUAL_DELTA(157.5 * lumi/1468404, xSectionProvider.getWeight(DataType::ttbar), 0.00000001);
     }
 
 };
 
 extern cute::suite make_suite_TestCrossSectionProvider() {
     cute::suite s;
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedTTbarEventsFor20pb));
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedWjetsEventsFor20pb));
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedZjetsEventsFor20pb));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedTTbarEvents));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedWjetsEvents));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedZjetsEvents));
 
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_EMEnriched_Pt20to30EventsFor20pb));
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_EMEnriched_Pt30to80EventsFor20pb));
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_EMEnriched_Pt80to170EventsFor20pb));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_EMEnriched_Pt20to30Events));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_EMEnriched_Pt30to80Events));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_EMEnriched_Pt80to170Events));
 
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_BCtoE_Pt20to30EventsFor20pb));
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_BCtoE_Pt30to80EventsFor20pb));
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_BCtoE_Pt80to170EventsFor20pb));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_BCtoE_Pt20to30Events));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_BCtoE_Pt30to80Events));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedQCD_BCtoE_Pt80to170Events));
 
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedVQQEventsFor20pb));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedVQQEvents));
 
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedsingleTop_And_WEventsFor20pb));
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedsingleTopTChannelEventsFor20pb));
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedsingleTopSChannelEventsFor20pb));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedsingleTop_And_WEvents));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedsingleTopTChannelEvents));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedsingleTopSChannelEvents));
 
-    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedDataEventsFor20pb));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetExpectedDataEvents));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetWeightData));
+    s.push_back(CUTE_SMEMFUN(TestCrossSectionProvider, testGetWeightTtbar));
 
     return s;
 }
