c3f7856a8f240c1c1a746936099df586fc79ffda
kreczko <>
2010-10-12 15:16:43 +0000

    added new TestObject definitions

--- BAT/test/TestObjectFactory.h
+++ BAT/test/TestObjectFactory.h
@@ -8,6 +8,7 @@
 #ifndef TESTOBJECTFACTORY_H_
 #define TESTOBJECTFACTORY_H_
 #include "../interface/Event.h"
+#include "../interface/RecoObjects/PrimaryVertex.h"
 #include "../interface/RecoObjects/Jet.h"
 #include "../interface/RecoObjects/Electron.h"
 #include "../interface/RecoObjects/Track.h"
@@ -17,9 +18,14 @@ class TestObjectFactory {
 public:
     TestObjectFactory();
     virtual ~TestObjectFactory();
+    static BAT::PrimaryVertexPointer goodPrimaryVertex();
+    static BAT::PrimaryVertexPointer badFakePrimaryVertex();
     static BAT::TrackPointer highPurityTrack();
     static BAT::TrackPointer lowPurityTrack();
+    static BAT::ElectronPointer goodCaloElectron();
+    static BAT::ElectronPointer goodPFElectron();
     static BAT::ElectronPointer goodIsolatedElectron();
+    static BAT::ElectronPointer goodIsolatedElectron2();
     static BAT::ElectronPointer goodLooseElectron();
     static BAT::ElectronPointer badElectron();
     static BAT::ElectronPointer electronWithMissingInnerLayerHit();
