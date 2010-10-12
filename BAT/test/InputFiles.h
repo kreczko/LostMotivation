5b881762fc05ebbdf6897fa4b6e70b3578eac5bc
kreczko <>
2010-10-12 14:31:48 +0000

    created a file to gather all inputs

---
+++ BAT/test/InputFiles.h
@@ -0,0 +1,32 @@
+/*
+ * InputFiles.h
+ *
+ *  Created on: 11 Oct 2010
+ *      Author: kreczko
+ */
+
+#ifndef INPUTFILES_H_
+#define INPUTFILES_H_
+namespace InputFile {
+const char* ttbar = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_ttjet/e20skim_nTuple_ttjet_f_1_2_zoz.root";
+const char* wjets = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_wjet/e20skim_nTuple_wj_f_1_1_Khg.root";
+const char* zjets = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_zjet/e20skim_nTuple_zj_1_1_I17.root";
+const char* enri1 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri1/e20skim_nTuple_enri1_f_1_2_jqe.root";
+const char* enri2 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri2/e20skim_nTuple_enri2_f_1_1_nWb.root";
+const char* enri3 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri3/e20skim_nTuple_enri3_f_1_3_jHm.root";
+const char* bce1 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_bce1/e20skim_nTuple_bce1_1_1_gV9.root";
+const char* bce2 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_bce2/e20skim_nTuple_bce2_1_1_L9D.root";
+const char* bce3 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_bce3/e20skim_nTuple_bce3_1_2_PfY.root";
+const char* tW = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_tW/e20skim_nTuple_tW_f_1_4_VPY.root";
+const char* tchan = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_tchan/e20skim_nTuple_tchan_f_1_1_hEK.root";
+const char* pj1 = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj1/e20skim_nTuple_pj1_f_1_1_gwd.root";
+const char* pj2 = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj2/e20skim_nTuple_pj2_f_1_1_jJm.root";
+const char* pj3 = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj3/e20skim_nTuple_pj3_f_10_1_cHS.root";
+
+const char* data =
+        "/storage/top/data_38X/e20skim/e20skim_Electron_Run2010B-PromptReco-v2_AOD_3/e20skim_nTuple_data_1_1_HHo.root";
+const char* data2 =
+        "/storage/top/data_38X/e20skim/e20skim_Electron_Run2010B-PromptReco-v2_AOD_3/e20skim_nTuple_data_2_1_XXb.root";
+}
+
+#endif /* INPUTFILES_H_ */
