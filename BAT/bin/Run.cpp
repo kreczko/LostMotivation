af1f597eebf00ca1033c8c7a2817a7790a0e8bb6
kreczko <>
2010-10-12 15:10:19 +0000

    added 38X data

--- BAT/bin/Run.cpp
+++ BAT/bin/Run.cpp
@@ -26,15 +26,18 @@ int main(int argc, char **argv) {
     setUpOnce();
     TStopwatch watch;
     watch.Start();
-    Analysis::useJetAlgorithm(Jet::ParticleFlow);
+    Analysis::useJetAlgorithm(Jet::Calo_AntiKT_Cone05);
     Analysis::useElectronAlgorithm(Electron::Calo);
-    Analysis::useMETAlgorithm(MET::ParticleFlowMET);
+    Analysis::useMETAlgorithm(MET::Calo);
+    Analysis::luminosity = 7.22;
 
     boost::scoped_ptr<Analysis> myAnalysis(new Analysis());
     //    myAnalysis->setMaximalNumberOfEvents(10000);
     myAnalysis->setUsedNeutrinoSelectionForTopPairReconstruction(NeutrinoSelectionCriterion::chi2);
 
-    //data
+    //Synchronisation exercise:
+    myAnalysis->addInputFile("/storage/top/mc/sync/*.root");
+    //data Run2010A
 //    myAnalysis->addInputFile("/storage/top/data/e20skim/e20skim_EG_Run2010A-Jul16-v4_RECO_139559_140159/*.root");
 //    myAnalysis->addInputFile("/storage/top/data/e20skim/e20skim_EG_Run2010A-Jun14thReReco_v1_RECO/*.root");
 //    myAnalysis->addInputFile("/storage/top/data/e20skim/e20skim_EG_Run2010A-PromptReco-v4/*.root");
@@ -45,10 +48,18 @@ int main(int argc, char **argv) {
 //    myAnalysis->addInputFile("/storage/top/data/e20skim_EG_Run2010A-PromptReco-v4_143657_143835/*.root");
 //    myAnalysis->addInputFile("/storage/top/data/e20skim_EG_Run2010A-PromptReco-v4_143953_144114/*.root");
     //--------------- 2.9pb-1 until here --------------------------------------------------
+    //--------new lumi count
+    //data Run2010A 38X
+//    myAnalysis->addInputFile("/storage/top/data_38X/e20skim/e20skim_Run2010A-Sep17ReReco/*.root");
+//3.1pb-1 till here
+//Run2010B 38X
+//    myAnalysis->addInputFile("/storage/top/data_38X/e20skim/e20skim_Electron_Run2010B-PromptReco-v2_AOD_3/*.root");
+//    myAnalysis->addInputFile("/storage/top/data_38X/e20skim/e20skim_Electron_Run2010B-PromptReco-v2_AOD_146804_147116/*.root");
+//    myAnalysis->addInputFile("/storage/top/data_38X/e20skim/e20skim_Electron_Run2010B-PromptReco-v2_AOD_147116_extra/*.root");
 //MC
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_ttjet/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_wjet/*.root");
-//    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_zj/*.root");
+//    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_zjet/*.root");
     //QCD
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_bce1/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_bce2/*.root");
@@ -56,6 +67,10 @@ int main(int argc, char **argv) {
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri1/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri2/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri3/*.root");
+    // photon + jets
+//    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj1/*.root");
+//    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj2/*.root");
+//    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj3/*.root");
     //Vector boson + heavy flavour
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_vqq/*.root");
     //Zprime samples
@@ -65,7 +80,7 @@ int main(int argc, char **argv) {
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_Zprime_M1TeV_W10GeV/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_Zprime_M1TeV_W100GeV/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_Zprime_M1250GeV_W12500MeV/*.root");
-    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_Zprime_M1500GeV_W15GeV/*.root");
+//    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_Zprime_M1500GeV_W15GeV/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_Zprime_M1500GeV_W150GeV/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_Zprime_M2TeV_W20GeV/*.root");
 //    myAnalysis->addInputFile("/storage/top/mc/spring10_7TeV_v5/MG/e20skim_Zprime_M2TeV_W200GeV/*.root");
