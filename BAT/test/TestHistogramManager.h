cdbdcd681f7710f150052cd2dc4e5e01cced68af
kreczko <>
2010-11-01 15:42:25 +0000

    added tests for 2D and jetbinned histograms now removing created files after each test

--- BAT/test/TestHistogramManager.h
+++ BAT/test/TestHistogramManager.h
@@ -21,21 +21,24 @@ private:
 public:
     TestHistogramManager() :
         man(), lumi(1.5) {
-        boost::array<bool, DataType::NUMBER_OF_DATA_TYPES> seenDataTypes = boost::array<bool, DataType::NUMBER_OF_DATA_TYPES>();
+        boost::array<bool, DataType::NUMBER_OF_DATA_TYPES> seenDataTypes = boost::array<bool,
+                DataType::NUMBER_OF_DATA_TYPES>();
 
-//        for (unsigned int type = 0; type < DataType::NUMBER_OF_DATA_TYPES; ++type) {
-//            seenDataTypes[type] = false;
-//        }
         seenDataTypes[DataType::DATA] = true;
         seenDataTypes[DataType::ttbar] = true;
 
         man.setCurrentLumi(lumi);
-        man.prepairForSeenDataTypes(seenDataTypes);
+        man.prepareForSeenDataTypes(seenDataTypes);
         man.addH1D("myHist", "Testing", 10, 0., 1);
+        man.addH2D("myHist2D", "Testing", 10, 0., 1, 10, 0., 1);
+        man.addH1D_JetBinned("myHistJeted", "Testing", 10, 0., 1);
     }
 
     ~TestHistogramManager() {
-
+        if (boost::filesystem::exists("data_1.5pb.root"))
+            boost::filesystem::remove("data_1.5pb.root");
+        if (boost::filesystem::exists("ttjet_1.5pb.root"))
+            boost::filesystem::remove("ttjet_1.5pb.root");
     }
 
     void testPreparationData() {
@@ -50,8 +53,24 @@ public:
 
     void testDataFile() {
         man.writeToDisk();
-        boost::filesystem::path path("data_1.5pb.root");
-        ASSERT(boost::filesystem::exists(path));
+        ASSERT(boost::filesystem::exists("data_1.5pb.root"));
+    }
+
+    void testPreparationData2D() {
+        man.setCurrentDataType(DataType::DATA);
+        ASSERT(man.H2D("myHist2D") != 0);
+    }
+
+    void testPreparationDatajetBinned() {
+        man.setCurrentDataType(DataType::DATA);
+        man.setCurrentJetBin(0);
+        ASSERT(man.H1D_JetBinned("myHistJeted_0jet") != 0);
+    }
+
+    void testNumberOfHistsInFile(){
+        man.writeToDisk();
+        boost::shared_ptr<TFile> file(new TFile("data_1.5pb.root"));
+        ASSERT_EQUAL(2 + JetBin::NUMBER_OF_JET_BINS, file->GetNkeys());
     }
 
 };
@@ -61,6 +80,9 @@ extern cute::suite make_suite_TestHistogramManager() {
     s.push_back(CUTE_SMEMFUN(TestHistogramManager, testPreparationData));
     s.push_back(CUTE_SMEMFUN(TestHistogramManager, testPreparationTtbar));
     s.push_back(CUTE_SMEMFUN(TestHistogramManager, testDataFile));
+    s.push_back(CUTE_SMEMFUN(TestHistogramManager, testPreparationData2D));
+    s.push_back(CUTE_SMEMFUN(TestHistogramManager, testPreparationDatajetBinned));
+    s.push_back(CUTE_SMEMFUN(TestHistogramManager, testNumberOfHistsInFile));
     return s;
 }
 
