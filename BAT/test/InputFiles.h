f5eeac7d76cd169730f6ba2ce354efe913eb3a52
kreczko <>
2010-11-09 15:38:55 +0000

    changed input files to the reprocessed 36X MC in 38X

--- BAT/test/InputFiles.h
+++ BAT/test/InputFiles.h
@@ -8,25 +8,25 @@
 #ifndef INPUTFILES_H_
 #define INPUTFILES_H_
 namespace InputFile {
-const char* ttbar = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_ttjet/e20skim_nTuple_ttjet_f_1_2_zoz.root";
-const char* wjets = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_wjet/e20skim_nTuple_wj_f_1_1_Khg.root";
-const char* zjets = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_zjet/e20skim_nTuple_zj_1_1_I17.root";
-const char* enri1 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri1/e20skim_nTuple_enri1_f_1_2_jqe.root";
-const char* enri2 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri2/e20skim_nTuple_enri2_f_1_1_nWb.root";
-const char* enri3 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_enri3/e20skim_nTuple_enri3_f_1_3_jHm.root";
+const char* ttbar = "/storage/top/mc/38Xrereco/e20skim_TTbarJets-madgraph_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* wjets = "/storage/top/mc/38Xrereco/e20skim_WJets-madgraph_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* zjets = "/storage/top/mc/38Xrereco/e20skim_ZJets-madgraph_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* enri1 = "/storage/top/mc/38Xrereco/e20skim_QCD_EMEnriched_Pt20to30_Spring10-START3X_V26_S09-v1/*_2_1_*.root";
+const char* enri2 = "/storage/top/mc/38Xrereco/e20skim_QCD_EMEnriched_Pt30to80_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* enri3 = "/storage/top/mc/38Xrereco/e20skim_QCD_EMEnriched_Pt80to170_Spring10-START3X_V26_S09-v3/*_1_1_*.root";
 const char* bce1 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_bce1/e20skim_nTuple_bce1_1_1_gV9.root";
-const char* bce2 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_bce2/e20skim_nTuple_bce2_1_1_L9D.root";
-const char* bce3 = "/storage/top/mc/spring10_7TeV_v5/pythia/e20skim_bce3/e20skim_nTuple_bce3_1_2_PfY.root";
-const char* tW = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_tW/e20skim_nTuple_tW_f_1_4_VPY.root";
-const char* tchan = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_tchan/e20skim_nTuple_tchan_f_1_1_hEK.root";
-const char* pj1 = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj1/e20skim_nTuple_pj1_f_1_1_gwd.root";
-const char* pj2 = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj2/e20skim_nTuple_pj2_f_1_1_jJm.root";
-const char* pj3 = "/storage/top/mc/spring10_7TeV_v5/MG/e20skim_pj3/e20skim_nTuple_pj3_f_10_1_cHS.root";
+const char* bce2 = "/storage/top/mc/38Xrereco/e20skim_QCD_BCtoE_Pt30to80_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* bce3 = "/storage/top/mc/38Xrereco/e20skim_QCD_BCtoE_Pt80to170_Spring10-START3X_V26_S09-v1/*_1_2_*.root";
+const char* tW = "/storage/top/mc/38Xrereco/e20skim_SingleTop_tWChannel-madgraph_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* tchan = "/storage/top/mc/38Xrereco/e20skim_SingleTop_tChannel-madgraph_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* pj1 = "/storage/top/mc/38Xrereco/e20skim_PhotonJets_Pt40to100-madgraph_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* pj2 = "/storage/top/mc/38Xrereco/e20skim_PhotonJets_Pt100to200-madgraph_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
+const char* pj3 = "/storage/top/mc/38Xrereco/e20skim_PhotonJets_Pt200toInf-madgraph_Spring10-START3X_V26_S09-v1/*_1_1_*.root";
 
 const char* data =
-        "/storage/top/data_38X/e20skim/e20skim_Electron_Run2010B-PromptReco-v2_AOD_3/e20skim_nTuple_data_1_1_HHo.root";
+        "/storage/top/data_38X/e20skim_Electron_Run2010B-PromptReco-v2_AOD_148822_148864/*_1_1_*.root";
 const char* data2 =
-        "/storage/top/data_38X/e20skim/e20skim_Electron_Run2010B-PromptReco-v2_AOD_3/e20skim_nTuple_data_2_1_XXb.root";
+        "/storage/top/data_38X/e20skim/e20skim_Electron_Run2010B-PromptReco-v2_AOD_3/*_2_2_*.root";
 }
 
 #endif /* INPUTFILES_H_ */
