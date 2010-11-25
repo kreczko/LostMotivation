fe2df32127dbb2b52afbe9a3cc49ffe988ae9780
kreczko <>
2010-11-25 22:54:00 +0000

    removed obsolete code

--- BNT/test/nTupleMaker_MC_cfg.py
+++ BNT/test/nTupleMaker_MC_cfg.py
@@ -1,12 +1,3 @@
-import FWCore.ParameterSet.Config as cms
-# Starting with a skeleton process which gets imported with the following line
-
-#import sys
-#print sys.argv
-import os
-# Select input type ("Data", "Data_EG", "MinBias_MC" or "Spring10_Physics_MC")
-os.environ['selection'] = "Data_EG"
-
 from BristolAnalysis.NTupleTools.runBasics_MC_cfi import process
 
 process.source.fileNames = cms.untracked.vstring(
@@ -19,3 +10,5 @@ process.ak5PFL2Relative.useCondDB = False
 process.ak5PFL3Absolute.useCondDB = False
 process.ak5PFResidual.useCondDB = False
 process.TFileService.fileName = 'testNtupler_MC.root'
+
+process.maxEvents.input = -1 
