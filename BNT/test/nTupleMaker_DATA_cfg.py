d25c47f8d1c9336528a290cbbb94fbbc16d56b37
kreczko <>
2010-12-08 20:46:33 +0000

    JEC are now taken from CondDB

--- BNT/test/nTupleMaker_DATA_cfg.py
+++ BNT/test/nTupleMaker_DATA_cfg.py
@@ -3,12 +3,6 @@ from BristolAnalysis.NTupleTools.runBasics_DATA_cfi import process
 process.source.fileNames = cms.untracked.vstring(
             'file:/storage/top/Nov4ReReco_example.root'
             )
-process.ak5CaloL2Relative.useCondDB = False
-process.ak5CaloL3Absolute.useCondDB = False
-process.ak5CaloResidual.useCondDB = False
-process.ak5PFL2Relative.useCondDB = False
-process.ak5PFL3Absolute.useCondDB = False
-process.ak5PFResidual.useCondDB = False
 process.TFileService.fileName = 'testNtupler_DATA.root'
 
 process.maxEvents.input = 100
--- BNT/test/nTupleMaker_MC_cfg.py
+++ BNT/test/nTupleMaker_MC_cfg.py
@@ -3,12 +3,6 @@ from BristolAnalysis.NTupleTools.runBasics_MC_cfi import process
 process.source.fileNames = cms.untracked.vstring(
             'file:/storage/top/mc/ttbarAODSIM38X.root'
             )
-process.ak5CaloL2Relative.useCondDB = False
-process.ak5CaloL3Absolute.useCondDB = False
-process.ak5CaloResidual.useCondDB = False
-process.ak5PFL2Relative.useCondDB = False
-process.ak5PFL3Absolute.useCondDB = False
-process.ak5PFResidual.useCondDB = False
 process.TFileService.fileName = 'testNtupler_MC.root'
 
 process.maxEvents.input = 100
