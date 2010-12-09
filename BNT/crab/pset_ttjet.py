032b35dc2d45fc03e86ad10ca1b5c1ed1bea66ab
kreczko <>
2010-12-09 16:40:06 +0000

    added configurations for W+jets and ttJets

---
+++ BNT/crab/pset_ttjet.py
@@ -0,0 +1,20 @@
+import FWCore.ParameterSet.Config as cms
+
+from BristolAnalysis.NTupleTools.runBasics_MC_cfi import process
+
+process.source.fileNames = cms.untracked.vstring(
+            'file:/storage/top/mc/ttbarAODSIM38X.root'
+            )
+# reduce stdout
+process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(10000)
+#process.MessageLogger.cerr.INFO.limit = 10
+#process.MessageLogger.cerr.threshold = 'ERROR'
+
+process.options.SkipEvent = cms.untracked.vstring('ProductNotFound')
+
+# rename output file
+process.TFileService.fileName = 'nTuple_ttjet.root'
+process.out.fileName          = 'pat_ttjet.root'
+#use online skimming
+process.LJFilter.electronsMin = 1
+process.LJFilter.elecPT = 25.
