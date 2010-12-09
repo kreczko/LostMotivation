a9b189ad566441b87162f60396f33431881fc79e
kreczko <>
2010-12-09 17:41:33 +0000

    added configs for QCD EMEnriched samples

---
+++ BNT/crab/pset_enri2.py
@@ -0,0 +1,5 @@
+from pset_default_MC import *
+
+# rename output file
+process.TFileService.fileName = 'nTuple_enri2.root'
+process.out.fileName          = 'pat_enri2.root'
---
+++ BNT/crab/pset_enri3.py
@@ -0,0 +1,5 @@
+from pset_default_MC import *
+
+# rename output file
+process.TFileService.fileName = 'nTuple_enri3.root'
+process.out.fileName          = 'pat_enri3.root'
