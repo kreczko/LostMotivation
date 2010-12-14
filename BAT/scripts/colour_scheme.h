9a3b2f7dc309b934b912afeb54c1c87cf2dc6247
kreczko <>
2010-12-14 17:03:01 +0000

    added missing scripts

---
+++ BAT/scripts/colour_scheme.h
@@ -0,0 +1,36 @@
+//void define_colours(){
+
+  //-------------------------------------
+  // my colour scheme
+  //-------------------------------------
+  //   Color_t Col_sig  = kRed-7;
+  //   Color_t Col_qcd  = kAzure-9;
+  //   Color_t Col_wj   = kGreen-9;
+  //   Color_t Col_zj   = kYellow-7;
+  //   Color_t Col_vqq  = kGray+1;
+  //   Color_t Col_stop = kViolet-4;
+  //   Color_t Col_pj   = kGray+1;
+  //-------------------------------------
+
+  //-------------------------------------
+  // Top PAG colour scheme
+  // ref: https://twiki.cern.ch/twiki/bin/view/CMS/TopQuarkPlotStyles
+  //-------------------------------------
+  Color_t Col_sig  = kRed+1;
+  Color_t Col_qcd  = kYellow;
+  Color_t Col_wj   = kGreen-3;
+  Color_t Col_zj   = kAzure-2;
+  Color_t Col_stop = kMagenta;
+  Color_t Col_pj   = kGray+1;
+  //Color_t Col_vqq  = kGray+1;
+  //Color_t Col_pj   = kGray+1;
+  
+  Style_t Sty_sig  = 1001;
+  Style_t Sty_qcd  = 1001;
+  Style_t Sty_wj   = 1001;
+  Style_t Sty_zj   = 1001;//3231; //3331; //changed from 3001
+  Style_t Sty_pj   = 1001;//
+  Style_t Sty_stop = 1001;//3004; //3018; //3318 ?
+  //-------------------------------------
+
+//}
