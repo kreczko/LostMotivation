c196f9b6c94ec4c106fccb6fb539c67154e2e686
kreczko <>
2011-01-07 20:03:20 +0000

    added single top CRAB configurations

---
+++ BNT/crab/pset_tW.py
@@ -0,0 +1,5 @@
+from pset_default_MC import *
+
+# rename output file
+process.TFileService.fileName = 'nTuple_tWchan.root'
+process.out.fileName          = 'pat_tWchan.root'
---
+++ BNT/crab/pset_tchan.py
@@ -0,0 +1,5 @@
+from pset_default_MC import *
+
+# rename output file
+process.TFileService.fileName = 'nTuple_tchan.root'
+process.out.fileName          = 'pat_tchan.root'
