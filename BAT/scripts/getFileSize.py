9df9f20c844a103f1453001b309d47278a5c40d5
kreczko <>
2011-01-24 15:05:40 +0000

    moved common functions to extra file

--- BAT/scripts/getFileSize.py
+++ BAT/scripts/getFileSize.py
@@ -3,11 +3,7 @@ from __future__ import division
 import os
 import sys
 import glob
-
-def getFiles(path):
-    path += "/*.root"
-    files = glob.glob(path)
-    return files
+from fileInfo import *
 
 def getFileSizes(files):# < 3 GB
     totalSize = 0
@@ -27,15 +23,15 @@ def getFileSizes(files):# < 3 GB
 if __name__ == "__main__":
     args = sys.argv
     if not len(args) == 2:
-        print "Please specify a folder to merge files in."
+        print "Please specify a folder to analyze."
         sys.exit()
         
     path = sys.argv[1]
-    files = getFiles(path)
+    files = getROOTFiles(path)
     sizes = getFileSizes(files)
     print 'Total size of files: %.3f MB' % (sizes['total']/10**6)
     print 'Number of files: %d'% len(files)
     print 'Minimal file size: %.3f MB'% (sizes['min']/10**6)
     print 'Maximal file size: %.3f MB'% (sizes['max']/10**6)
     print 'Average file size: %.3f MB'% (sizes['total']/10**6/len(files))
-    print 'target max file size: 1GB', '=> scale factor =', (10**9)/sizes['max']
+    print 'target max file size: 250MB', '=> scale factor =', (10**9)/4/sizes['max']
