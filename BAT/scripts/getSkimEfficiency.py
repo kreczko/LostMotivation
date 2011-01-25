1c14b9df330f8a4dd602357d46cb70631ca625b7
kreczko <>
2011-01-25 16:45:18 +0000

    correction of the skim information, now HBHE noise filter is included

--- BAT/scripts/getSkimEfficiency.py
+++ BAT/scripts/getSkimEfficiency.py
@@ -7,23 +7,44 @@ from ROOT import *
 from mergeROOTFilesWithCompression import getProcess
 
 pathToSkimHist = "LJFilter/EventCount/EventCounter"
+pathToEventTree = "rootTupleTree/tree"
 
-def getSkimInfo(files):
-    skimHist = getSkimHist(files)
+def getSkimmedEvents(files):
+    skimInfo = getSkimInfo(files)
+    skimHist = skimInfo[0]
+    
     numberOfTotalEvents = skimHist.GetBinContent(1)
     numberOfSkimmedEvents = skimHist.GetBinContent(2)
-    skimEfficiency = numberOfSkimmedEvents/numberOfTotalEvents
-    return {'total': numberOfTotalEvents, 'skimmed':numberOfSkimmedEvents, 'efficiency':skimEfficiency}
+    numberOfEventsAfterNoiseFilter = skimInfo[1]
+    electronSkimEfficiency = numberOfSkimmedEvents/numberOfTotalEvents
+    TotalSkimEfficiency = numberOfEventsAfterNoiseFilter/numberOfTotalEvents
+    return {'total': numberOfTotalEvents, 'skimmed':numberOfSkimmedEvents, 
+            'electronSkimEfficiency':electronSkimEfficiency,
+            'afterSkimAndNoiseFilter':numberOfEventsAfterNoiseFilter,
+            'totalSkimEfficiency':TotalSkimEfficiency}
             
-def getSkimHist(files):
+def getSkimInfo(files):
     skimHist = None
-    
+    eventBranch = None
+    eventTree = None
+    n_skim = 0
     TfOpen = TFile.Open
     gcd = gROOT.cd
     
     for file in files:
         f = TfOpen(file)
         gcd()
+        eventTree = f.Get(pathToEventTree)
+        if eventTree is None:
+            print 'Could not find tree', pathToEventTree, 'in file', file
+        else:
+            eventBranch = eventTree.GetBranch('event')
+            
+        if eventBranch is None:
+            print 'Could not find branch', pathToEventTree + '/event', 'in file', file
+        else:
+            n_skim += eventBranch.GetEntries()
+        
         if skimHist is None:
             skimHist = f.Get(pathToSkimHist)
             if skimHist is None:
@@ -35,7 +56,7 @@ def getSkimHist(files):
                 print 'Could not find histogram', pathToSkimHist, 'in file', file
                 continue
             skimHist.Add(hist)
-    return skimHist
+    return (skimHist, n_skim)
             
 
 if __name__ == "__main__":
@@ -46,10 +67,21 @@ if __name__ == "__main__":
         
     path = sys.argv[1]
     files = getROOTFiles(path)
-    events = getSkimInfo(files)
-    
-    print 'Total number of files:', len(files)
-    print 'Process recognised:', getProcess(files[0])
-    print 'Total number of events:', int(events['total'])
-    print 'Skimmed number of events:', int(events['skimmed'])
-    print 'Skim efficiency:', events['efficiency']
+    events = getSkimmedEvents(files)
+    print '='*100
+    print '{0:70} : {1:15d}'.format('Total number of files', len(files))
+    print '{0:70} : {1:15s}'.format('Process recognised', getProcess(files[0]))
+    print '{0:70} : {1:15d}'.format('Total number of events', int(events['total']))
+    print '{0:70} : {1:15d}'.format('Number of events after electron skim', int(events['skimmed']))
+    print '{0:70} : {1:15d}'.format('Number of events after electron skim and HBHE noise filter', events['afterSkimAndNoiseFilter'])
+    print
+    print '{0:70} : {1:15f}'.format('Electron skim efficiency', events['electronSkimEfficiency'])
+    print '{0:70} : {1:15f}'.format('Total skim efficiency', events['totalSkimEfficiency'])
+    print '='*100
+#    print 'Total number of files:', len(files)
+#    print 'Process recognised:', getProcess(files[0])
+#    print 'Total number of events:', int(events['total'])
+#    print 'Number of events after electron skim:', int(events['skimmed'])
+#    print 'Electron skim efficiency:', events['electronSkimEfficiency']
+#    print 'Number of events after electron skim and HBHE noise filter:', events['afterSkimAndNoiseFilter']
+#    print 'Total skim efficiency:', events['totalSkimEfficiency']
