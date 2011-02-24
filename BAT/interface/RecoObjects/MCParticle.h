455be16b50f568dab666d1c8e58d55febe9cf2f3
senkin <>
2011-02-24 14:39:41 +0000

    PdgId, MotherIndex and Status added

--- BAT/interface/RecoObjects/MCParticle.h
+++ BAT/interface/RecoObjects/MCParticle.h
@@ -10,12 +10,26 @@
 #include "Particle.h"
 
 namespace BAT {
+class MCParticle;
+typedef boost::shared_ptr<MCParticle> MCParticlePointer;
+typedef std::vector<MCParticlePointer> MCParticleCollection;
 
-class MCParticle : public Particle{
+class MCParticle: public Particle {
 public:
-	float id;
+	int pdgId() const;
+	int motherIndex() const;
+	int status() const;
 	MCParticle();
+	MCParticle(float energy, float px, float py, float pz);
+	//MCParticle(int pdgId, float phi, float eta, float pt, float energy, int motherIndex, int status);
 	virtual ~MCParticle();
+	void setPdgId(int PdgId);
+	void setMotherIndex(int MotherIndex);
+	void setStatus(int Status);
+protected:
+	int particlePdgId;
+	int particleMotherIndex;
+	int particleStatus;
 };
 
 }
--- BAT/src/RecoObjects/MCParticle.cpp
+++ BAT/src/RecoObjects/MCParticle.cpp
@@ -9,11 +9,54 @@
 
 namespace BAT {
 
-MCParticle::MCParticle(): Particle() {
+MCParticle::MCParticle() : Particle(),
+	particlePdgId(0), particleMotherIndex(0), particleStatus(0){
 
 }
+
+MCParticle::MCParticle(float energy, float px, float py, float pz): Particle(energy, px, py, pz),
+		particlePdgId(0), particleMotherIndex(0), particleStatus(0){
+
+}
+//MCParticle::Particle(const MCParticle& particle) :
+//	particlePdgId(particle.pdgId()), particleMotherIndex(particle.motherIndex()), particleStatus(particle.status) {
+//
+//}
+
+//MCParticle::Particle(int pdgId, float phi, float eta, float pt, float energy, int motherIndex, int status) {
+//
+//}
+
+//MCParticle::Particle(): Particle() {
+//
+//}
 
 MCParticle::~MCParticle() {
 }
+
+int MCParticle::pdgId() const {
+	return particlePdgId;
+}
+
+int MCParticle::motherIndex() const {
+	return particleMotherIndex;
+}
+
+int MCParticle::status() const {
+	return particleStatus;
+}
+
+void MCParticle::setStatus(int Status) {
+	particleStatus = Status;
+}
+
+void MCParticle::setMotherIndex(int MotherIndex){
+	particleMotherIndex = MotherIndex;
+}
+
+void MCParticle::setPdgId(int PdgId){
+	particlePdgId = PdgId;
+}
+
 
 }
