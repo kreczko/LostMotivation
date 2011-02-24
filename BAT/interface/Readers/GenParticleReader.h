5db89e2480af4f4b4d82ab3ea2a7e9832cfbb605
senkin <>
2011-02-24 14:38:53 +0000

    first version of GenParticleReader

---
+++ BAT/interface/Readers/GenParticleReader.h
@@ -0,0 +1,37 @@
+/*
+ * GenParticleReader.h
+ *
+ *  Created on: Feb 23, 2011
+ *      Author: phzss
+ */
+
+#ifndef GENPARTICLEREADER_H_
+#define GENPARTICLEREADER_H_
+#include "../RecoObjects/MCParticle.h"
+#include "VariableReader.h"
+
+namespace BAT {
+
+class GenParticleReader {
+private:
+	VariableReader<MultiIntPointer> pdgIdReader;
+    VariableReader<MultiDoublePointer> pxReader;
+    VariableReader<MultiDoublePointer> pyReader;
+    VariableReader<MultiDoublePointer> pzReader;
+    VariableReader<MultiDoublePointer> energyReader;
+    VariableReader<MultiIntPointer> statusReader;
+    VariableReader<MultiIntPointer> motherIndexReader;
+
+    MCParticleCollection genParticles;
+    void readGenParticles();
+public:
+    GenParticleReader();
+    GenParticleReader(TChainPointer input);
+    virtual ~GenParticleReader();
+    const MCParticleCollection& getGenParticles();
+    void initialise();
+};
+
+}
+
+#endif /* GENPARTICLEREADER_H_ */
---
+++ BAT/src/Readers/GenParticleReader.cpp
@@ -0,0 +1,73 @@
+/*
+ * GenParticleReader.cpp
+ *
+ *  Created on: Feb 23, 2011
+ *      Author: phzss
+ */
+
+#include "../../interface/Readers/GenParticleReader.h"
+namespace BAT {
+
+GenParticleReader::GenParticleReader() :
+	pdgIdReader(),
+	pxReader(),
+	pyReader(),
+	pzReader(),
+	energyReader(),
+	statusReader(),
+	motherIndexReader() {
+
+}
+
+GenParticleReader::GenParticleReader(TChainPointer input) :
+	pdgIdReader(input, "GenParticle.PdgId"),
+    pxReader(input, "GenParticle.Px"),
+    pyReader(input, "GenParticle.Py"),
+    pzReader(input, "GenParticle.Pz"),
+    energyReader(input, "GenParticle.Energy"),
+    statusReader(input, "GenParticle.Status"),
+    motherIndexReader(input, "GenParticle.MotherIndex") {
+
+}
+
+
+GenParticleReader::~GenParticleReader() {
+}
+
+const MCParticleCollection& GenParticleReader::getGenParticles() {
+    if (genParticles.empty() == false)
+        genParticles.clear();
+    readGenParticles();
+    return genParticles;
+}
+
+void GenParticleReader::readGenParticles() {
+    for (unsigned int index = 0; index < pxReader.size(); index++) {
+    	int pdgId = pdgIdReader.getIntVariableAt(index);
+        float px = pxReader.getVariableAt(index);
+        float py = pyReader.getVariableAt(index);
+        float pz = pzReader.getVariableAt(index);
+        float energy = energyReader.getVariableAt(index);
+        int motherIndex = motherIndexReader.getIntVariableAt(index);
+        int status = statusReader.getIntVariableAt(index);
+
+        MCParticlePointer genParticle(new MCParticle(energy, px, py, pz));
+        genParticle->setPdgId(pdgId);
+        genParticle->setMotherIndex(motherIndex);
+        genParticle->setStatus(status);
+
+        genParticles.push_back(genParticle);
+    }
+}
+
+
+void GenParticleReader::initialise() {
+	pdgIdReader.initialise();
+	pxReader.initialise();
+    pyReader.initialise();
+    pzReader.initialise();
+    energyReader.initialise();
+    statusReader.initialise();
+}
+
+}
