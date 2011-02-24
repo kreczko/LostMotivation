f7a1801ff7bb9ce48da84ec5783e38d5b2dbab8c
kreczko <>
2011-02-24 17:55:55 +0000

    updated location of test files

--- BNT/test/nTupleMaker_DATA_cfg.py
+++ BNT/test/nTupleMaker_DATA_cfg.py
@@ -1,7 +1,7 @@
 from BristolAnalysis.NTupleTools.runBasics_DATA_cfi import process
 
 process.source.fileNames = cms.untracked.vstring(
-            'file:/storage/top/Nov4ReReco_example.root'
+            'file:/storage/TopQuarkGroup/data/Nov4ReReco_example.root'
             )
 process.TFileService.fileName = 'testNtupler_DATA.root'
 
